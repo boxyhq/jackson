fa5228fee6ef48497671ea11b006bc9c3b4ff070
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2021-09-01 20:19:58 +0100

    parse IdP metadata file

--- src/saml.js
+++ src/saml.js
@@ -1,5 +1,6 @@
 var saml = require('@boxyhq/saml20');
 var xml2js = require('xml2js');
+var rambda = require('rambda');
 
 module.exports = {
   parse: async function (rawAssertion) {
@@ -40,7 +41,26 @@ module.exports = {
           return;
         }
 
-        resolve(res);
+        const entityID = rambda.path('EntityDescriptor.$.entityID', res);
+        let X509Certificate = null;
+
+        const ssoDes = rambda.path('EntityDescriptor.IDPSSODescriptor', res);
+        for (let i = 0; i < ssoDes.length; ++i) {
+          const keyDes = ssoDes[i]['KeyDescriptor'];
+          for (let j = 0; j < keyDes.length; ++j) {
+            if (keyDes[j]['$'] && keyDes[j]['$'].use === 'signing') {
+              const ki = keyDes[j]['KeyInfo'][0];
+              const cd = ki['X509Data'][0];
+              X509Certificate = cd['X509Certificate'][0];
+            }
+          }
+        }
+
+        // TODO: SingleSignOnService
+        resolve({
+          entityID,
+          X509Certificate,
+        });
       });
     });
   },
