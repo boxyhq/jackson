627c02ecca4d94beb5db843610db91ca7083bcd2
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2021-09-16 11:28:07 +0100

    fixed typo

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 const hostUrl = process.env.HOST_URL || 'localhost';
 const hostPort = (process.env.HOST_PORT || '5000') * 1;
 const externalUrl =
-  process.env.EXTRNAL_URL || 'http://' + hostUrl + ':' + hostPort;
+  process.env.EXTERNAL_URL || 'http://' + hostUrl + ':' + hostPort;
 const samlAudience = process.env.SAML_AUDIENCE || 'https://auth.boxyhq.com';
 
 const internalUrl = process.env.HOST_URL || 'localhost';
