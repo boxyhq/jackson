00c14afed94ad86090e8262d2408df21d4649973
Aswin V <vaswin91@gmail.com>
2021-12-28 23:18:34 +0530

    saml files ts conversion

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -132,12 +132,19 @@ export interface DatabaseOption {
 }
 
 export interface SAMLReq {
-  ssoUrl: string;
+  ssoUrl?: string;
   entityID: string;
   callbackUrl: string;
-  isPassive: boolean;
-  forceAuthn: boolean;
-  identifierFormat: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress';
-  providerName: 'BoxyHQ';
+  isPassive?: boolean;
+  forceAuthn?: boolean;
+  identifierFormat?: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress';
+  providerName?: 'BoxyHQ';
   signingKey: string;
+}
+
+export interface SAMLProfile {
+  audience: string;
+  claims: Record<string, any>;
+  issuer: string;
+  sessionIndex: string;
 }
