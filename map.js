b397c0f5f333c28f687949e0d9d4a6b3dd1a12b3
Kiran <kiran@boxyhq.com>
2021-12-31 12:42:21 +0530

    Fix the test coverage

--- map.js
+++ map.js
@@ -1,1 +1,11 @@
-export default (test) => test.replace(/\.test\.js$/, '.js');
+// module.exports = (test) => test.replace(/\.test\.ts$/, '.ts');
+
+const map = {
+  'api.test.ts': ['src/controller/api.ts'],
+  'oauth.test.ts': ['src/controller/oauth.ts', 'src/controller/oauth/*'],
+  'db.test.ts': ['src/db/*'],
+};
+
+module.exports = (testFile) => {
+  return map[testFile.split('/')[2]];
+};
