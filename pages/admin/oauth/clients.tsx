70c3c3b894667c89b1454e4b8a7bd542dd17a0b4
Aswin V <vaswin91@gmail.com>
2022-01-13 22:44:48 +0530

    Loading state handling

--- pages/admin/oauth/clients.tsx
+++ pages/admin/oauth/clients.tsx
@@ -1,31 +1,39 @@
 import { NextPage } from 'next';
 import useSWR from 'swr';
 import { fetcher } from '@lib/utils';
+import Link from 'next/link';
 
 const OAuthClient: NextPage = () => {
   const { data, error } = useSWR('/api/admin/providers', fetcher);
 
   if (error) {
     return (
-      <div className='p-6'>
-        <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded'>
-          {JSON.stringify(error.info)}
-        </div>
+      <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded'>
+        {error.info ? JSON.stringify(error.info) : error.status}
       </div>
     );
   }
 
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   if (!Array.isArray(data)) {
     return (
-      <div className='p-6'>
+      <div>
         <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded'>Nothing to show</div>
       </div>
     );
   }
 
   return (
-    <div className='p-6'>
-      <table className='border-collapse w-full border-y border-gray-200 dark:border-gray-500 bg-white dark:bg-gray-800 text-sm shadow-sm'>
+    <div>
+      <Link href={'/admin/oauth/new'}>
+        <a className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded leading-6 inline-block'>
+          Configure IdP
+        </a>
+      </Link>
+      <table className='border-collapse w-full border-y border-gray-200 dark:border-gray-500 bg-white dark:bg-gray-800 text-sm shadow-sm mt-6'>
         <thead className='bg-gray-50 dark:bg-gray-700'>
           <tr>
             <th className='w-1/2 border-b border-gray-300 dark:border-gray-600 font-semibold p-4 text-gray-900 dark:text-gray-200 text-left'>
