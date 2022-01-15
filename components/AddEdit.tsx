1086d939fcad2bfbc004b0a3c95e613729fa80c4
Aswin V <vaswin91@gmail.com>
2022-01-15 21:18:01 +0530

    Reusable component for add/edit

---
+++ components/AddEdit.tsx
@@ -0,0 +1,228 @@
+import { ArrowLeftIcon } from '@heroicons/react/outline';
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { FormEvent, useState } from 'react';
+import type { IdPConfig } from '@lib/jackson';
+
+const basicFields = [
+  { id: 'name', label: 'Name', required: true, type: 'text', placeholder: 'MyApp' },
+  {
+    id: 'tenant',
+    label: 'Tenant',
+    required: true,
+    type: 'text',
+    placeholder: 'acme.com',
+  },
+  {
+    id: 'product',
+    label: 'Product',
+    required: true,
+    type: 'text',
+    placeholder: 'demo',
+  },
+];
+
+const settingsFields = [
+  {
+    id: 'redirectUrl',
+    label: 'Allowed redirect URLs (,Comma separated)',
+    required: true,
+    type: 'textarea',
+    placeholder: 'http://localhost:3000',
+    isArray: true,
+  },
+  {
+    id: 'defaultRedirectUrl',
+    label: 'Default redirect URL',
+    required: true,
+    type: 'url',
+    placeholder: 'http://localhost:3000/login/saml',
+  },
+  {
+    id: 'rawMetadata',
+    label: 'Raw IdP XML',
+    required: true,
+    type: 'textarea',
+    placeholder: 'Paste the raw XML here',
+  },
+];
+
+function getInitialState(client) {
+  const _state: IdPConfig = {} as IdPConfig;
+  basicFields.forEach(({ id, isArray = false }: { isArray?: boolean } & typeof basicFields[number]) => {
+    _state[id] = client?.[id] ? (isArray ? client[id].join() : client[id]) : '';
+  });
+  settingsFields.forEach(({ id, isArray = false }: { isArray?: boolean } & typeof basicFields[number]) => {
+    _state[id] = client?.[id] ? (isArray ? client[id].join() : client[id]) : '';
+  });
+  return _state;
+}
+
+type AddEditProps = {
+  client?: Record<string, unknown>;
+};
+
+const AddEdit = ({ client }: AddEditProps) => {
+  const saveIdPConfig = async (event) => {
+    event.preventDefault();
+    const { rawMetadata, redirectUrl, ...rest } = formObj;
+    const encodedRawMetadata = btoa(rawMetadata || '');
+    const redirectUrlList = redirectUrl.split(',');
+
+    await fetch('/api/admin/providers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Api-Key secret',
+      },
+      body: JSON.stringify({ ...rest, encodedRawMetadata, redirectUrl: JSON.stringify(redirectUrlList) }),
+    });
+  };
+
+  const [activeTab, setActiveTab] = useState(0);
+
+  // form state
+  const [formObj, setFormObj] = useState<IdPConfig>(() => getInitialState(client));
+  function handleChange(event: FormEvent) {
+    const target = event.target as HTMLInputElement | HTMLTextAreaElement;
+    setFormObj((cur) => ({ ...cur, [target.id]: target.value }));
+  }
+
+  return (
+    <>
+      {/* Or use router.back()  */}
+      <Link href='/admin/saml/clients'>
+        <a className='inline-flex items-center px-4 py-2 mt-2 md:leading-6 text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300'>
+          <ArrowLeftIcon aria-hidden className='h-4 w-4 text-black dark:text-slate-50' />
+          <span className='ml-2'>Back to Clients</span>
+        </a>
+      </Link>
+      <div className='mb-4 border-b border-gray-200 dark:border-gray-700'>
+        <ul className='flex flex-wrap -mb-px' id='myTab' data-tabs-toggle='#samlClients' role='tablist'>
+          <li className='mr-2' role='presentation'>
+            <button
+              className={`${
+                activeTab === 0 ? 'active ' : ''
+              }inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300`}
+              id='basic-info-tab'
+              data-tabs-target='#basic-info'
+              type='button'
+              role='tab'
+              aria-controls='basic-info'
+              aria-selected={activeTab === 0}
+              onClick={() => setActiveTab(0)}>
+              Basic information
+            </button>
+          </li>
+          <li className='mr-2' role='presentation'>
+            <button
+              className={`${
+                activeTab === 1 ? 'active ' : ''
+              }inline-block py-4 px-4 text-sm font-medium text-center text-gray-500 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300`}
+              id='settings-tab'
+              data-tabs-target='#settings'
+              type='button'
+              role='tab'
+              aria-controls='settings'
+              aria-selected={activeTab === 1}
+              onClick={() => setActiveTab(1)}>
+              Settings
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div id='samlClients'>
+        <form onSubmit={saveIdPConfig}>
+          <div
+            className={`${
+              activeTab === 0 ? '' : 'hidden '
+            }bg-gradient-to-r bg-white border border-gray-200 code-preview dark:bg-gray-800 dark:border-gray-700 p-6 rounded-xl md:w-3/4 min-w-[28rem] md:max-w-lg`}
+            role='tabpanel'
+            id='basic-info'
+            aria-labelledby='basic-info-tab'>
+            {basicFields.map(({ id, placeholder, label, type, required }) => (
+              <div className='mb-6 ' key={id}>
+                <label
+                  htmlFor={id}
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  {label}
+                </label>
+                {type === 'textarea' ? (
+                  <textarea
+                    id={id}
+                    placeholder={placeholder}
+                    value={formObj[id]}
+                    onChange={handleChange}
+                    className='block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    rows={3}
+                  />
+                ) : (
+                  <input
+                    id={id}
+                    type={type}
+                    placeholder={placeholder}
+                    value={formObj[id]}
+                    onChange={handleChange}
+                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    required={required}
+                  />
+                )}
+              </div>
+            ))}
+
+            <button
+              type='submit'
+              className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded leading-6 inline-block'>
+              Save Changes
+            </button>
+          </div>
+          <div
+            role='tabpanel'
+            id='settings'
+            aria-labelledby='settings-tab'
+            className={`${
+              activeTab === 1 ? '' : 'hidden '
+            }bg-gradient-to-r bg-white border border-gray-200 code-preview dark:bg-gray-800 dark:border-gray-700 p-6 rounded-xl md:w-3/4 min-w-[28rem] md:max-w-lg`}>
+            {settingsFields.map(({ id, placeholder, label, type, required }) => (
+              <div className='mb-6 ' key={id}>
+                <label
+                  htmlFor={id}
+                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  {label}
+                </label>
+                {type === 'textarea' ? (
+                  <textarea
+                    id={id}
+                    placeholder={placeholder}
+                    className='block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    rows={3}
+                    value={formObj[id]}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  <input
+                    id={id}
+                    type={type}
+                    placeholder={placeholder}
+                    className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                    required={required}
+                    value={formObj[id]}
+                    onChange={handleChange}
+                  />
+                )}
+              </div>
+            ))}
+
+            <button
+              type='submit'
+              className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded leading-6 inline-block'>
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddEdit;
---
+++ pages/admin/saml/edit/[id].tsx
@@ -0,0 +1,27 @@
+import { NextPage } from 'next';
+import useSWR from 'swr';
+import { fetcher } from '@lib/utils';
+import AddEdit from '@components/AddEdit';
+import { useRouter } from 'next/router';
+
+const EditClient: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useSWR('/api/admin/providers', fetcher);
+
+  if (error) {
+    return (
+      <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded'>
+        {error.info ? JSON.stringify(error.info) : error.status}
+      </div>
+    );
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+  const idpClient = data.find(({ clientID }) => id === clientID);
+  return <AddEdit client={idpClient} />;
+};
+
+export default EditClient;
