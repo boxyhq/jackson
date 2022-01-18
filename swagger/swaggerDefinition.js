3a21507b5f1e7143b4a5c6c5ad1920cb6759bf1a
Kiran <kiran@boxyhq.com>
2022-01-18 20:36:16 +0530

    Add swagger spec

---
+++ swagger/swaggerDefinition.js
@@ -0,0 +1,34 @@
+const package = require('../package.json');
+
+module.exports = {
+  info: {
+    title: 'SAML Jackson API',
+    version: package.version,
+    description: 'This is the API documentation for SAML Jackson service.',
+    termsOfService: 'https://boxyhq.com/terms.html',
+    contact: {
+      name: 'Boxy HQ',
+      url: 'https://boxyhq.com/',
+      email: 'deepak@boxyhq.com',
+    },
+    license: {
+      name: 'Apache-2.0 License',
+      url: 'https://github.com/boxyhq/jackson/blob/main/LICENSE',
+    },
+  },
+  host: 'localhost:5000',
+  basePath: '/',
+  schemes: ['http', 'https'],
+  securityDefinitions: {
+    apiKey: {
+      type: 'apiKey',
+      in: 'header',
+      name: 'Authorization',
+    },
+  },
+  security: [
+    {
+      apiKey: [],
+    },
+  ],
+};
--- swaggerDefinition.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  info: {
-    title: 'Hello World',
-    version: '1.0.0',
-    description: 'A sample API',
-  },
-  host: 'www.example.com',
-  basePath: '/',
-};
