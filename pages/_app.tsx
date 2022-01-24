1ac4207d846ab4ee47b2d973edfa48d02cebdb75
Aswin V <vaswin91@gmail.com>
2022-01-24 12:06:59 +0530

    Remove flowbite [cleanup]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,8 @@ import Layout from '@components/Layout';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    import('@themesberg/flowbite');
-  }, []);
   return (
     <SessionProvider session={pageProps.session}>
       <Layout>
