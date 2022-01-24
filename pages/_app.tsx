ad24e10d8e2db758eff43eb4b593d77396cbd5a4
Aswin V <vaswin91@gmail.com>
2022-01-24 23:24:57 +0530

    Destructure session first

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
