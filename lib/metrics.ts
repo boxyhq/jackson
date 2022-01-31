c9a35c35de0b73ec713f80a8a51a29e2b3f97f40
Kiran <kiran@boxyhq.com>
2022-02-01 00:40:19 +0530

    Add otel protocol

--- lib/metrics.ts
+++ lib/metrics.ts
@@ -1,15 +1,20 @@
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
 import { MeterProvider } from '@opentelemetry/sdk-metrics-base';
 import { metrics } from '@opentelemetry/api-metrics';
+import { Resource } from '@opentelemetry/resources';
+import { name, version } from '../package.json';
+
+const exporter = new OTLPMetricExporter({
+  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+});
 
 const meterProvider = new MeterProvider({
-  exporter: new OTLPMetricExporter({}),
+  exporter: exporter,
   interval: 1000,
+  resource: new Resource({
+    'service.version': `${version}`,
+    'service.name': `${name}`
+  })
 });
 
 metrics.setGlobalMeterProvider(meterProvider);
-
-export {
-  metrics,
-  meterProvider
-}
