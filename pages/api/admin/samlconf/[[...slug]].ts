df6f5b9d8e5bda33f7687d6b2734405ab8c6794f
Aswin V <vaswin91@gmail.com>
2022-02-02 21:46:24 +0530

    Handle getAll and getConfig using slug

--- pages/api/admin/samlconf/[[...slug]].ts
+++ pages/api/admin/samlconf/[[...slug]].ts
@@ -14,7 +14,12 @@ export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { adminController, apiController } = await jackson();
     if (req.method === 'GET') {
-      res.json(await adminController.getAllConfig());
+      const { slug } = req.query;
+      if (slug?.[0]) {
+        res.json(await apiController.getConfig({ clientID: slug[0] }));
+      } else {
+        res.json(await adminController.getAllConfig());
+      }
     } else if (req.method === 'POST') {
       res.json(await apiController.config(req.body));
     } else if (req.method === 'PATCH') {
