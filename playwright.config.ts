6bdeb399219e86650bf3d343900e0bed1db6f801
Aswin V <vaswin91@gmail.com>
2022-02-08 12:09:19 +0530

    Use prod build

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,13 @@ const config: PlaywrightTestConfig = {
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: 'npm run mongo',
+    command: 'npm run build && npm run start',
+    env: {
+      JACKSON_API_KEYS: 'secret',
+      DB_ENGINE: 'sql',
+      DB_TYPE: 'postgres',
+      DB_URL: 'postgres://postgres:postgres@localhost:5432/jackson',
+    },
     port: 5000,
     timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
