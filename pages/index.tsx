0f2da78aac5427725f70dbf9bb59fed15c9f9b4d
Aswin V <vaswin91@gmail.com>
2022-02-14 20:59:19 +0530

    Redirect to saml config route

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/admin');
+    router.push('/admin/saml/config');
   }, [router]);
 
   return <p>Redirecting...</p>;
