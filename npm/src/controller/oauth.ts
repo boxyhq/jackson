59426da8e683795f934578c54bc6b2868d67fa0e
Deepak Prabhakara <deepak@boxyhq.com>
2022-02-21 11:58:48 +0000

    delete token after use

--- npm/src/controller/oauth.ts
+++ npm/src/controller/oauth.ts
@@ -428,6 +428,13 @@ export class OAuthController implements IOAuthController {
       throw new JacksonError('Invalid token', 403);
     }
 
+    // delete the token
+    try {
+      await this.tokenStore.delete(token);
+    } catch (_err) {
+      // ignore error
+    }
+
     return rsp.claims;
   }
 }
