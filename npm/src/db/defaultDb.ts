d395ff05139a9af0d2f1a1e45012f50e1ae7fd8e
Aswin V <vaswin91@gmail.com>
2022-03-15 11:08:14 +0530

    Use defaultDb util across npm and service

---
+++ npm/src/db/defaultDb.ts
@@ -0,0 +1,12 @@
+import { JacksonOption } from '../typings';
+
+export default function defaultDb(opts: JacksonOption) {
+  opts.db = opts.db || {};
+  opts.db.engine = opts.db.engine || 'sql';
+  opts.db.url = opts.db.url || 'postgresql://postgres:postgres@localhost:5432/postgres';
+  opts.db.type = opts.db.type || 'postgres'; // Only needed if DB_ENGINE is sql.
+  opts.db.ttl = (opts.db.ttl || 300) * 1; // TTL for the code, session and token stores (in seconds)
+  opts.db.cleanupLimit = (opts.db.cleanupLimit || 1000) * 1; // Limit cleanup of TTL entries to this many items at a time
+
+  return opts;
+}
