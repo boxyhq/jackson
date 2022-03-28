40d5496ba58ead96405f6a2ccf95401ad87b1001
Aswin V <vaswin91@gmail.com>
2022-03-28 12:52:44 +0530

    Minor formatting fix

--- pages/error.tsx
+++ pages/error.tsx
@@ -17,6 +17,7 @@ export default function Error() {
     // clear the cookie once read
     deleteCookie(JACKSON_ERROR_COOKIE_KEY, pathname);
   }, [pathname]);
+
   const { statusCode, message } = error;
   let statusText = '';
   if (typeof statusCode === 'number') {
