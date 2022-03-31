b40504f8535f32ae407a1e4f03f14aad18b456a7
Deepak Prabhakara <deepak@boxyhq.com>
2022-03-31 12:28:18 +0100

    verbose logging for migrations

--- npm/typeorm.ts
+++ npm/typeorm.ts
@@ -7,7 +7,7 @@ const AppDataSource = new DataSource(<DataSourceOptions>{
   url: process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5432/postgres',
   synchronize: false,
   migrationsTableName: '_jackson_migrations',
-  logging: ['error'],
+  logging: ['all'],
   entities: ['src/db/sql/entity/**/*.ts'],
   migrations: [`migration/${type}/**/*.ts`],
 });
