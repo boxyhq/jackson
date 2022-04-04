0c7819be1b0049759be3e6ac3e18638ae598511b
Vishal Lodha <vishal@boxyhq.com>
2022-04-04 16:15:14 +0530

    merging db name with URL

--- npm/migrate-mongo-config.js
+++ npm/migrate-mongo-config.js
@@ -1,7 +1,6 @@
 const config = {
   mongodb: {
-    url: process.env.DB_URL || 'mongodb://localhost:27017/',
-    databaseName: 'jackson',
+    url: process.env.DB_URL || 'mongodb://localhost:27017/jackson',
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
       useUnifiedTopology: true, // removes a deprecating warning when connecting
