6851fffcdbeddaed66a5a3d11729cd7c914bab7f
Aswin V <vaswin91@gmail.com>
2022-04-25 12:58:48 +0530

    Support for array value [idpList]

--- npm/src/controller/oauth/redirect.ts
+++ npm/src/controller/oauth/redirect.ts
@@ -1,11 +1,12 @@
-export const success = (
-  redirectUrl: string,
-  params: Record<string, string>
-): string => {
+export const success = (redirectUrl: string, params: Record<string, string | string[]>): string => {
   const url: URL = new URL(redirectUrl);
 
   for (const [key, value] of Object.entries(params)) {
-    url.searchParams.set(key, value);
+    if (Array.isArray(value)) {
+      value.forEach((v) => url.searchParams.append(key, v));
+    } else {
+      url.searchParams.set(key, value);
+    }
   }
 
   return url.href;
