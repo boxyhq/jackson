89c27db79119a53a5ffca130aa46b82741ffbcf2
Aswin V <vaswin91@gmail.com>
2022-04-25 17:24:22 +0530

    Filter our undefined values

--- npm/src/controller/oauth/redirect.ts
+++ npm/src/controller/oauth/redirect.ts
@@ -1,10 +1,13 @@
-export const success = (redirectUrl: string, params: Record<string, string | string[]>): string => {
+export const success = (
+  redirectUrl: string,
+  params: Record<string, string | string[] | undefined>
+): string => {
   const url: URL = new URL(redirectUrl);
 
   for (const [key, value] of Object.entries(params)) {
     if (Array.isArray(value)) {
       value.forEach((v) => url.searchParams.append(key, v));
-    } else {
+    } else if (value !== undefined) {
       url.searchParams.set(key, value);
     }
   }
