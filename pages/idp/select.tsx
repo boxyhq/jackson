13ece89b2fc3dea50ebfd6899550ca99b650feb0
Aswin V <vaswin91@gmail.com>
2022-04-26 16:54:45 +0530

    Switch from form to link

--- pages/idp/select.tsx
+++ pages/idp/select.tsx
@@ -5,48 +5,29 @@ export default function IdPSelection() {
 
   const { idp: idpList, ...rest } = router.query as { idp?: string[] };
 
-  const paramsToRelay = Object.entries(rest);
+  const paramsToRelay = new URLSearchParams(Object.entries(rest));
 
   if (!Array.isArray(idpList)) {
     return <div>IdP list missing</div>;
   }
 
   return (
-    <form
-      action='/api/oauth/authorize'
-      className='relative top-1/2 left-1/2  w-1/2 max-w-xl  -translate-x-1/2 -translate-y-1/2 overflow-auto text-center'>
-      {paramsToRelay
-        .filter(([, value]) => value !== undefined)
-        .map(([key, value]) => (
-          <input key={key} type='hidden' name={key} value={value} />
-        ))}
-      <fieldset className='border-8 p-4'>
-        <legend className='px-3 text-left text-black'>Select an Identity Provider</legend>
-        <div className='max-h-96 overflow-auto'>
-          {idpList?.map((idp) => {
-            const { clientID, provider } = JSON.parse(idp);
-            return (
-              <div className='relative my-3 bg-white' key={clientID}>
-                <input
-                  id={`radio-${clientID}`}
-                  name='idp_hint'
-                  type='radio'
-                  className={`peer sr-only`}
-                  value={clientID}
-                />
-                <label
-                  htmlFor={`radio-${clientID}`}
-                  className='relative block w-full cursor-pointer overflow-hidden py-3 px-8 text-center text-[#3C454C] transition-colors peer-checked:bg-primary/25'>
-                  {provider}
-                </label>
-              </div>
-            );
-          })}
-        </div>
-        <button type='submit' className='btn-primary'>
-          Proceed
-        </button>
-      </fieldset>
-    </form>
+    <div className='relative top-1/2 left-1/2  w-1/2 max-w-xl  -translate-x-1/2 -translate-y-1/2 overflow-auto rounded-md border-[1px] py-4 px-6 text-center'>
+      <h1 className='mb-4 px-3 text-center text-lg font-bold text-black'>Choose an Identity Provider</h1>
+      <ul className='max-h-96 overflow-auto'>
+        {idpList?.map((idp) => {
+          const { clientID, provider } = JSON.parse(idp);
+          return (
+            <li className='relative my-3 border-b-[1px] bg-white last:border-b-0' key={clientID}>
+              <a
+                href={`/api/oauth/authorize?${paramsToRelay.toString()}&idp_hint=${clientID}`}
+                className='relative block w-full cursor-pointer overflow-hidden  py-3 px-8 text-center text-[#3C454C] outline-none transition-colors hover:bg-primary/10 focus:bg-primary/30'>
+                {provider}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
