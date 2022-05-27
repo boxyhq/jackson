7da52796941cc3ad1e5f3bd7e9ad2bdb1f5dbba5
Deepak Prabhakara <deepak@boxyhq.com>
2022-05-27 23:54:20 +0100

    WIP

--- npm/typeorm.ts
+++ npm/typeorm.ts
@@ -4,13 +4,16 @@ import { DataSource, DatabaseType, DataSourceOptions } from 'typeorm';
 const type = <DatabaseType>process.env.DB_TYPE || <DatabaseType>'postgres';
 
 const AppDataSource = new DataSource(<DataSourceOptions>{
-  type,
+  type: type === <DatabaseType>'planetscale' ? 'mysql' : type,
   url: process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5432/postgres',
   synchronize: false,
   migrationsTableName: '_jackson_migrations',
   logging: 'all',
   entities: ['src/db/sql/entity/**/*.ts'],
   migrations: [`migration/${type}/**/*.ts`],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default AppDataSource;
