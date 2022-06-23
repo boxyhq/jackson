f0ffb74090e2168a79c0f56a6c6030b6f8dd2da0
Aswin V <vaswin91@gmail.com>
2022-06-23 15:42:28 +0700

    Update metadata

--- pages/api/.well-known/openid-configuration.ts
+++ pages/api/.well-known/openid-configuration.ts
@@ -1,14 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import env from '../../../lib/env';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
-    issuer: 'https://example.com',
+    issuer: env.samlAudience,
     authorization_endpoint: 'https://example.com/oauth2/authorize',
-    token_endpoint: '',
-    userinfo_endpoint: '',
-    jwks_uri: '',
-    response_types_supported: '',
-    subject_types_supported: '',
-    id_token_signing_alg_values_supported: '',
+    token_endpoint: `${env.externalUrl}/api/oauth/token`,
+    userinfo_endpoint: `${env.externalUrl}/api/oauth/userinfo`,
+    jwks_uri: `${env.externalUrl}/api/jwks`,
+    response_types_supported: ['code'],
+    subject_types_supported: ['public'],
+    id_token_signing_alg_values_supported: ['RS256'],
   });
 }
