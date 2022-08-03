f2f0ed8a3499a0fd2432c1672c996f2a52047e81
Deepak Prabhakara <deepak@boxyhq.com>
2022-08-04 00:01:23 +0100

    one more attempt

--- e2e/admin.spec.ts
+++ e2e/admin.spec.ts
@@ -3,5 +3,7 @@ import { test } from '@playwright/test';
 test('MAGIC_LINK in globalSetup should log me in', async ({ page }) => {
   await page.goto('/admin/saml/config');
   // Find an element with the text 'SAML Connections' and click on it
-  await page.waitForSelector('text=SAML Connections');
+  await page.waitForSelector('text="SAML Connections"', {
+    state: 'visible',
+  });
 });
