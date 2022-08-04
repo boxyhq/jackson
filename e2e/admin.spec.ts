022cead1880cdf575994065e2c83aee549caea79
Kiran <kiran@boxyhq.com>
2022-08-04 07:52:27 +0530

    Fix the e2e test

--- e2e/admin.spec.ts
+++ e2e/admin.spec.ts
@@ -3,6 +3,6 @@ import { test } from '@playwright/test';
 test('MAGIC_LINK in globalSetup should log me in', async ({ page }) => {
   await page.goto('/admin/saml/config');
 
-  // Find an element with the text 'SAML Connections' and click on it
-  await page.locator('data-test-id=create-saml-connections').click();
+  // Find the button and click on it
+  await page.locator('data-test-id=create-saml-connection').click();
 });
