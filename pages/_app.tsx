9f18f9699b9e0e37d1a5b737e1c01a511328fce7
Kiran <kiran@boxyhq.com>
2022-08-04 08:21:11 +0530

    Adjust the layout loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 import { AccountLayout } from '@components/layouts';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const { pathname } = useRouter();
+
+  if (pathname !== '/' && !pathname.startsWith('/admin')) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <SessionProvider session={session}>
       <AccountLayout>
