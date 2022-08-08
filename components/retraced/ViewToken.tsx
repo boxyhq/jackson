396faeb0384a8079b8b2c9445d43974941b3ac44
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2022-08-08 13:48:47 +0530

    minor changes

--- components/retraced/ViewToken.tsx
+++ components/retraced/ViewToken.tsx
@@ -1,24 +1,18 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import {
-  ArrowLeftIcon,
-  ClipboardCopyIcon,
-  CheckIcon
-} from '@heroicons/react/outline';
+import { ArrowLeftIcon, ClipboardCopyIcon, CheckIcon } from '@heroicons/react/outline';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import {
-    NewProject
-} from '../../interfaces/project';
+import { NewProject } from '../../interfaces/project';
 
 const ViewToken = (props: NewProject) => {
-    console.log(props.project.tokens)
+  console.log(props.project.tokens);
   const showAPIKeys = props.project.tokens.length > 0;
   const [copied, setCopied] = useState<boolean>(false);
   const apiKeys = props.project.tokens;
   const showCopied = () => {
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
-  }
+  };
 
   return (
     <>
@@ -29,49 +23,52 @@ const ViewToken = (props: NewProject) => {
         </a>
       </Link>
       {showAPIKeys && (
-        <div className='mt-6 overflow-auto rounded-lg shadow-md'>
-          <table className='min-w-full'>
-            <thead className='bg-gray-50 shadow-md dark:bg-gray-700 sm:rounded-lg'>
-              <tr>
-                <th
-                  scope='col'
-                  className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400'>
-                  Product
-                </th>
-                <th
-                  scope='col'
-                  className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400'>
-                  Environment
-                </th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {apiKeys.map((token) => (
-                <tr
-                  key={token.environment_id}
-                  className='border-b bg-white dark:border-gray-700 dark:bg-gray-800'>
-                  <td className='whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900 dark:text-white'>
-                    {token.name}
-                  </td>
-                  <td className='whitespace-nowrap px-6 py-4 text-sm text-gray-500 dark:text-gray-400'>
-                    {token.token}
-                  </td>
-                  <td>
-                    <a className='link-primary cursor-pointer'>
-                      <CopyToClipboard text={token.token} onCopy={showCopied}>
-                        <span>
-                          <ClipboardCopyIcon className='h-5 w-5 text-secondary' />
-                        </span>
-                      </CopyToClipboard>
-                    </a>
-                  </td>
+        <div>
+          <h2 className='mt-2 mb-4 text-3xl font-bold text-primary dark:text-white'>{props.project.name}</h2>
+          <div className='mt-6 overflow-auto rounded-lg shadow-md'>
+            <table className='min-w-full'>
+              <thead className='bg-gray-50 shadow-md dark:bg-gray-700 sm:rounded-lg'>
+                <tr>
+                  <th
+                    scope='col'
+                    className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400'>
+                    Token Name
+                  </th>
+                  <th
+                    scope='col'
+                    className='px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-700 dark:text-gray-400'>
+                    Token
+                  </th>
+                  <th></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="inline-flex items-center place-content-center">
-          {copied && <CheckIcon className='h-5 w-5 text-secondary'>Copied</CheckIcon>}
+              </thead>
+              <tbody>
+                {apiKeys.map((token) => (
+                  <tr
+                    key={token.environment_id}
+                    className='border-b bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <td className='whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900 dark:text-white'>
+                      {token.name}
+                    </td>
+                    <td className='whitespace-nowrap px-6 py-4 text-sm text-gray-500 dark:text-gray-400'>
+                      {token.token}
+                    </td>
+                    <td>
+                      <a className='link-primary cursor-pointer'>
+                        <CopyToClipboard text={token.token} onCopy={showCopied}>
+                          <span>
+                            <ClipboardCopyIcon className='h-5 w-5 text-secondary' />
+                          </span>
+                        </CopyToClipboard>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className='inline-flex place-content-center items-center'>
+              {copied && <CheckIcon className='h-5 w-5 text-secondary'>Copied</CheckIcon>}
+            </div>
           </div>
         </div>
       )}
