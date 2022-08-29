1ade4d42d1dda364dc6dde8ed3410ad8669bd77c
Aswin V <vaswin91@gmail.com>
2022-08-29 19:14:29 +0530

    Update test coverage map

--- npm/map.js
+++ npm/map.js
@@ -1,5 +1,5 @@
 const map = {
-  'test/api.test.ts': ['src/controller/api.ts'],
+  'test/api.test.ts': ['src/controller/api.ts', 'src/controller/connection/*'],
   'test/oauth.test.ts': ['src/controller/oauth.ts', 'src/controller/oauth/*', 'src/controller/utils.ts'],
   'test/logout.test.ts': ['src/controller/logout.ts', 'src/controller/utils.ts'],
   'test/db.test.ts': ['src/db/*'],
