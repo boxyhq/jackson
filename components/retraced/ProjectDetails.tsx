3c6a6caacfa7aaaeccd4fb550ec8175f1ac52bb6
Kiran <kiran@boxyhq.com>
2022-08-30 11:30:43 +0530

    Make the ProjectDetails 2 columns

--- components/retraced/ProjectDetails.tsx
+++ components/retraced/ProjectDetails.tsx
@@ -2,8 +2,8 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { ClipboardIcon } from '@heroicons/react/24/outline';
 import toast from 'react-hot-toast';
 
-import env from '@lib/env';
 import type { Project } from 'types';
+import env from '@lib/env';
 import CodeSnippet from '@components/retraced/CodeSnippet';
 
 const ProjectDetails = (props: { project: Project }) => {
@@ -14,38 +14,34 @@ const ProjectDetails = (props: { project: Project }) => {
 
   return (
     <>
-      <div className='grid grid-cols-1 md:grid-cols-2'>
-        <div className='space-y-3 border p-3'>
-          <div className='flex items-center'>
-            <div className='form-control w-full'>
-              <label className='label pl-0 font-semibold'>
-                <span className='label-text'>Project ID</span>
-                <ClipboardButton text={project.id} />
-              </label>
-              <input type='text' className='input input-bordered w-full' defaultValue={project.id} />
-            </div>
-          </div>
-          <div className='form-control w-full'>
-            <label className='label pl-0 font-semibold'>
-              <span className='label-text'>Publisher API Base URL</span>
-              <ClipboardButton text={baseUrl} />
-            </label>
-            <input type='text' className='input input-bordered w-full' defaultValue={baseUrl} />
-          </div>
-          <div className='form-control w-full'>
-            <label className='label pl-0 font-semibold'>
-              <span className='label-text'>{environments[0].name} Token</span>
-              <ClipboardButton text={tokens[0].token} />
-            </label>
-            <input type='text' className='input input-bordered w-full' defaultValue={tokens[0].token} />
-          </div>
-          <div className='form-control w-full'>
-            <label className='label pl-0 font-semibold'>
-              <span className='label-text'>{environments[1].name} Token</span>
-              <ClipboardButton text={tokens[1].token} />
-            </label>
-            <input type='text' className='input input-bordered w-full' defaultValue={tokens[1].token} />
-          </div>
+      <div className='grid grid-cols-1 gap-3 border p-3 md:grid-cols-2'>
+        <div className='form-control w-full'>
+          <label className='label pl-0 font-semibold'>
+            <span className='label-text'>Project ID</span>
+            <ClipboardButton text={project.id} />
+          </label>
+          <input type='text' className='input input-bordered w-full' defaultValue={project.id} />
+        </div>
+        <div className='form-control w-full'>
+          <label className='label pl-0 font-semibold'>
+            <span className='label-text'>Publisher API Base URL</span>
+            <ClipboardButton text={baseUrl} />
+          </label>
+          <input type='text' className='input input-bordered w-full' defaultValue={baseUrl} />
+        </div>
+        <div className='form-control w-full'>
+          <label className='label pl-0 font-semibold'>
+            <span className='label-text'>{environments[0].name} Token</span>
+            <ClipboardButton text={tokens[0].token} />
+          </label>
+          <input type='text' className='input input-bordered w-full' defaultValue={tokens[0].token} />
+        </div>
+        <div className='form-control w-full'>
+          <label className='label pl-0 font-semibold'>
+            <span className='label-text'>{environments[1].name} Token</span>
+            <ClipboardButton text={tokens[1].token} />
+          </label>
+          <input type='text' className='input input-bordered w-full' defaultValue={tokens[1].token} />
         </div>
       </div>
       <div className='mt-5 border p-3'>
