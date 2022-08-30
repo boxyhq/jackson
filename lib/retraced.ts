a270db9ccc48b8003b1cda6384642c6d0723cb68
Kiran <kiran@boxyhq.com>
2022-08-30 11:22:56 +0530

    Remove the unused method

--- lib/retraced.ts
+++ lib/retraced.ts
@@ -5,39 +5,6 @@ import type { AdminToken, Project, ApiResponse, Group } from 'types';
 import { fetcher } from '@lib/ui/utils';
 import env from './env';
 
-export const sanitizeResponse = (
-  projects: Project[]
-): Array<{
-  projectId: string;
-  name: string;
-  environmentId: string;
-  environmentName: string;
-  token: string;
-}> => {
-  const output: Array<{
-    projectId: string;
-    name: string;
-    environmentId: string;
-    environmentName: string;
-    token: string;
-  }> = [];
-
-  for (let i = 0; i < projects.length; i++) {
-    const row = projects[i];
-
-    for (let j = 0; j < projects[i].environments.length; j++) {
-      output.push({
-        projectId: row.id,
-        name: row.name,
-        environmentId: projects[i].environments[j].id,
-        environmentName: projects[i].environments[j].name,
-        token: projects[i].tokens.filter((t) => t.environment_id == projects[i].environments[j].id)[0].token,
-      });
-    }
-  }
-  return output;
-};
-
 export const getToken = async (): Promise<AdminToken> => {
   const { data } = await axios.post<{ adminToken: AdminToken }>(
     `${env.retraced.host}/admin/v1/user/_login`,
