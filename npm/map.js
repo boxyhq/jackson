cf70ddaf61ba824ee7885ffc4c7f2c7600424386
Aswin V <vaswin91@gmail.com>
2022-09-03 17:28:05 +0530

    Update map and cli

--- npm/map.js
+++ npm/map.js
@@ -1,7 +1,16 @@
 const map = {
-  'test/api.test.ts': ['src/controller/api.ts', 'src/controller/connection/*'],
-  'test/saml_idp.test.ts': ['src/controller/oauth.ts', 'src/controller/oauth/*', 'src/controller/utils.ts'],
-  'test/oidc_idp.test.ts': ['src/controller/oauth.ts', 'src/controller/oauth/*', 'src/controller/utils.ts'],
+  'test/saml_idp_api.test.ts': ['src/controller/api.ts', 'src/controller/connection/saml.ts'],
+  'test/oidc_idp_api.test.ts': ['src/controller/api.ts', 'src/controller/connection/oidc.ts'],
+  'test/saml_idp_oauth.test.ts': [
+    'src/controller/oauth.ts',
+    'src/controller/oauth/*',
+    'src/controller/utils.ts',
+  ],
+  'test/oidc_idp_oauth.test.ts': [
+    'src/controller/oauth.ts',
+    'src/controller/oauth/*',
+    'src/controller/utils.ts',
+  ],
   'test/logout.test.ts': ['src/controller/logout.ts', 'src/controller/utils.ts'],
   'test/db.test.ts': ['src/db/*'],
 };
