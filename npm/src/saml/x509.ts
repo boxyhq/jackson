cb1f4ff3bd8df32bd26f81b0de09a0b119cbbd95
Utkarsh Mehta <ukrocks.mehta@gmail.com>
2022-09-15 21:04:55 +0530

    certificate expiry date

--- npm/src/saml/x509.ts
+++ npm/src/saml/x509.ts
@@ -25,7 +25,7 @@ const generate = async () => {
     serialNumber: '01',
     name: 'CN=BoxyHQ Jackson',
     notBefore: new Date(),
-    notAfter: new Date('3021/01/01'), // TODO: set shorter expiry and rotate certificates
+    notAfter: new Date(new Date().setFullYear(new Date().getFullYear() + 27)),
     signingAlgorithm: alg,
     keys: keys,
     extensions,
