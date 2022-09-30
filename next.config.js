70bbe511f7c9dbd02f27e11dff85ffe72c05ab30
nafees nazik <nafeesnazik21@gmail.com>
2022-09-30 15:03:11 +0530

    feat: i18n config to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 
 module.exports = {
   reactStrictMode: true,
+  i18n,
   webpack: (config, { webpack, isServer }) => {
     if (isServer) {
       // Module not found
