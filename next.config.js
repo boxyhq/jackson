05eeb7f491b0c6e7cd8147c96272c4331a2a23e9
Nafees Nazik <84864519+G3root@users.noreply.github.com>
2022-10-08 23:24:43 +0530

    feat: add i18n internationalization (#560)
    * chore: add next-i18next
    
    * feat: add i8n-config
    
    * feat: i18n config to next config
    
    * feat: wrap app with the appWithTranslation
    
    * feat: add initial dummy translation
    
    * chore: pin version
    
    * feat: add translation
    
    Co-authored-by: Deepak Prabhakara <deepak@boxyhq.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const { i18n } = require('./next-i18next.config');
+
 /** @type {import('next').NextConfig} */
 
 module.exports = {
   reactStrictMode: true,
+  i18n,
   output: 'standalone',
   webpack: (config, { webpack, isServer }) => {
     if (isServer) {
