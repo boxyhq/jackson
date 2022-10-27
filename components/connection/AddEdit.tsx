1a120d7a4f152b0e71dd9631e5ad5b2bee22277a
Aswin V <vaswin91@gmail.com>
2022-10-27 21:39:46 +0530

    codemod changes for new-link transform in [components] directory

--- components/connection/AddEdit.tsx
+++ components/connection/AddEdit.tsx
@@ -267,11 +267,9 @@ const AddEdit = ({ connection }: AddEditProps) => {
 
   return (
     <>
-      <Link href='/admin/connection'>
-        <a className='btn btn-outline items-center space-x-2'>
-          <ArrowLeftIcon aria-hidden className='h-4 w-4' />
-          <span>Back</span>
-        </a>
+      <Link href='/admin/connection' className='btn-outline btn items-center space-x-2'>
+        <ArrowLeftIcon aria-hidden className='h-4 w-4' />
+        <span>Back</span>
       </Link>
       <div>
         <h2 className='mb-5 mt-5 font-bold text-gray-700 dark:text-white md:text-xl'>
@@ -370,13 +368,11 @@ const AddEdit = ({ connection }: AddEditProps) => {
                       {type === 'pre' ? (
                         <pre
                           className={`block w-full overflow-auto rounded-lg border border-gray-300 bg-gray-50 p-2 
-                        text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 
-                        dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 
-                        dark:focus:ring-blue-500 ${
-                          isHidden ? (isHidden(formObj[key]) == true ? 'hidden' : '') : ''
-                        } ${
-                            showWarning ? (showWarning(formObj[key]) ? 'border-2 border-rose-500' : '') : ''
-                          }`}>
+                      text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 
+                      dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 
+                      dark:focus:ring-blue-500 ${
+                        isHidden ? (isHidden(formObj[key]) == true ? 'hidden' : '') : ''
+                      } ${showWarning ? (showWarning(formObj[key]) ? 'border-2 border-rose-500' : '') : ''}`}>
                           {value}
                         </pre>
                       ) : type === 'textarea' ? (
@@ -388,7 +384,7 @@ const AddEdit = ({ connection }: AddEditProps) => {
                           readOnly={readOnly}
                           maxLength={maxLength}
                           onChange={getHandleChange()}
-                          className={`textarea textarea-bordered h-24 w-full ${
+                          className={`textarea-bordered textarea h-24 w-full ${
                             isArray ? 'whitespace-pre' : ''
                           }`}
                           rows={rows}
@@ -408,7 +404,7 @@ const AddEdit = ({ connection }: AddEditProps) => {
                             readOnly={readOnly}
                             maxLength={maxLength}
                             onChange={getHandleChange({ key: 'checked' })}
-                            className='checkbox checkbox-primary ml-5 align-middle'
+                            className='checkbox-primary checkbox ml-5 align-middle'
                           />
                         </>
                       ) : (
@@ -421,7 +417,7 @@ const AddEdit = ({ connection }: AddEditProps) => {
                           readOnly={readOnly}
                           maxLength={maxLength}
                           onChange={getHandleChange()}
-                          className='input input-bordered w-full'
+                          className='input-bordered input w-full'
                         />
                       )}
                     </div>
@@ -429,7 +425,7 @@ const AddEdit = ({ connection }: AddEditProps) => {
                 }
               )}
             <div className='flex'>
-              <button type='submit' className='btn btn-primary'>
+              <button type='submit' className='btn-primary btn'>
                 Save Changes
               </button>
               <p
@@ -461,7 +457,7 @@ const AddEdit = ({ connection }: AddEditProps) => {
               </div>
               <button
                 type='button'
-                className='btn btn-error'
+                className='btn-error btn'
                 onClick={toggleDelConfirm}
                 data-modal-toggle='popup-modal'>
                 Delete
