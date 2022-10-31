be5dff022ea01067e34f1d0c7e88049701904429
Kiran <kiran@boxyhq.com>
2022-10-31 12:03:34 +0530

    Add a new UI for the login screen

---
+++ pages/admin/auth/login.tsx
@@ -0,0 +1,106 @@
+import type { ReactElement } from 'react';
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import { useSession, getCsrfToken, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import toast from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
+import { useState } from 'react';
+import classNames from 'classnames';
+
+const Login = ({ csrfToken }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter();
+  const { status } = useSession();
+
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+
+  if (status === 'authenticated') {
+    router.push('/admin/connection');
+  }
+
+  const onSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    const response = await signIn('email', {
+      email,
+      csrfToken,
+      redirect: false,
+    });
+
+    setLoading(false);
+
+    if (!response) {
+      return;
+    }
+
+    const { error } = response;
+
+    if (error) {
+      toast.error(error);
+      return;
+    }
+
+    toast.success('A sign in link has been sent to your email address.');
+  };
+
+  return (
+    <div className='flex min-h-screen items-center justify-center bg-gray-100'>
+      <div className='mt-4 bg-white p-6 text-left shadow-md'>
+        <div className='space-y-3'>
+          <div className='flex justify-center'>
+            <Image src='/logo.png' alt='BoxyHQ logo' width={50} height={50} />
+          </div>
+          <h2 className='text-center text-3xl font-extrabold text-gray-900'>BoxyHQ Studio</h2>
+          <p className='text-center text-sm text-gray-600'>
+            Enterprise readiness for B2B SaaS, straight out of the box.
+          </p>
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className='mt-8'>
+            <div>
+              <label className='block' htmlFor='email'>
+                Email
+                <label>
+                  <input
+                    type='email'
+                    placeholder='Email'
+                    className='input-bordered input mb-5 mt-2 w-full rounded-md'
+                    required
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    value={email}
+                  />
+                </label>
+              </label>
+            </div>
+            <div className='flex items-baseline justify-between'>
+              <button
+                className={classNames('btn-primary btn-block btn rounded-md', loading ? 'loading' : '')}
+                type='submit'>
+                Send Magic Link
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+Login.getLayout = function getLayout(page: ReactElement) {
+  return <SessionProvider>{page}</SessionProvider>;
+};
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context),
+    },
+  };
+};
+
+export default Login;
