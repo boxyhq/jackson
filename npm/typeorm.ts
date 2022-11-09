e81de5cc9f352b0191bcd0623bc7dbad417fb77b
Deepak Prabhakara <deepak@boxyhq.com>
2022-11-09 22:27:11 +0000

    I1/mssql (#666)
    * mssql support
    
    * added mssql container for testing
    
    * added migration scripts for mssql
    
    * added mssql to dev docker-compose

--- npm/typeorm.ts
+++ npm/typeorm.ts
@@ -1,12 +1,14 @@
 require('reflect-metadata');
 import { DataSource, DatabaseType, DataSourceOptions } from 'typeorm';
+import * as mssql from './src/db/sql/mssql';
 
 const type =
   process.env.DB_ENGINE === 'planetscale'
     ? 'mysql'
     : <DatabaseType>process.env.DB_TYPE || <DatabaseType>'postgres';
 
-const entitiesDir = process.env.DB_ENGINE === 'planetscale' ? 'planetscale' : 'sql';
+const entitiesDir =
+  process.env.DB_ENGINE === 'planetscale' ? 'planetscale' : type === 'mssql' ? 'sql/mssql' : 'sql';
 const migrationsDir = process.env.DB_ENGINE === 'planetscale' ? 'planetscale' : type;
 
 let ssl;
@@ -16,15 +18,36 @@ if (process.env.DB_SSL === 'true') {
   };
 }
 
-const AppDataSource = new DataSource(<DataSourceOptions>{
+const url = process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5432/postgres';
+
+let AppDataSource: DataSource;
+
+const baseOpts = {
   type,
-  url: process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5432/postgres',
   synchronize: false,
   migrationsTableName: '_jackson_migrations',
   logging: 'all',
   entities: [`src/db/${entitiesDir}/entity/**/*.ts`],
   migrations: [`migration/${migrationsDir}/**/*.ts`],
-  ssl,
-});
+};
+
+if (type === 'mssql') {
+  const mssqlOpts = mssql.parseURL(url);
+  AppDataSource = new DataSource(<DataSourceOptions>{
+    host: mssqlOpts.host,
+    port: mssqlOpts.port,
+    database: mssqlOpts.database,
+    username: mssqlOpts.username,
+    password: mssqlOpts.password,
+    options: mssqlOpts.options,
+    ...baseOpts,
+  });
+} else {
+  AppDataSource = new DataSource(<DataSourceOptions>{
+    url: process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5432/postgres',
+    ssl,
+    ...baseOpts,
+  });
+}
 
 export default AppDataSource;
