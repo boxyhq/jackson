534ac49be0f5a156398d4c1321e054f2001da667
Kiran <kiran@boxyhq.com>
2022-11-15 00:23:44 +0530

    Add a loading state component

---
+++ components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className='flex items-center justify-center'>
+      <progress className='progress progress-primary w-56'></progress>
+    </div>
+  );
+};
+
+export default Loading;
