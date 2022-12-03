b3bfc9ff1999c17c8b4b231af82a61456d73bdc8
Deepak Prabhakara <deepak@boxyhq.com>
2022-12-03 23:01:29 +0000

    simplified counter creation

--- npm/src/opentelemetry/metrics.ts
+++ npm/src/opentelemetry/metrics.ts
@@ -1,50 +1,43 @@
 import { metrics } from '@opentelemetry/api';
 
-const counters = {
-  createConnection: {
-    name: 'jackson.connection.create',
-    description: 'Number of IdP connection create requests',
-  },
-
-  getConnections: {
-    name: 'jackson.connections.get',
-    description: 'Number of IdP connections get requests',
-  },
-
-  deleteConnections: {
-    name: 'jackson.connections.delete',
-    description: 'Number of IdP connections delete requests',
-  },
-
-  oauthAuthorize: {
-    name: 'jackson.oauth.authorize',
-    description: 'Number of oauth authorize requests',
-  },
-
-  oauthToken: {
-    name: 'jackson.oauth.token',
-    description: 'Number of oauth token requests',
-  },
-
-  oauthUserInfo: {
-    name: 'jackson.oauth.userinfo',
-    description: 'Number of oauth user info requests',
-  },
-};
-
-const createCounter = (action: string) => {
-  const meter = metrics.getMeterProvider().getMeter('jackson');
-  const counter = counters[action];
-
-  return meter.createCounter(counter.name, {
-    description: counter.description,
-  });
-};
+let meter = metrics.getMeterProvider().getMeter('jackson');
+let counters;
 
 const increment = (action: string) => {
-  const counter = createCounter(action);
-
-  counter.add(1, { provider: 'saml' });
+  const counter = counters[action];
+  if (counter) {
+    counter.add(1);
+  }
 };
 
-export { increment };
+const init = () => {
+  meter = metrics.getMeterProvider().getMeter('jackson');
+
+  counters = {
+    createConnection: meter.createCounter('jackson.connection.create', {
+      description: 'Number of IdP connection create requests',
+    }),
+
+    getConnections: meter.createCounter('jackson.connection.get', {
+      description: 'Number of IdP connections get requests',
+    }),
+
+    deleteConnections: meter.createCounter('jackson.connection.delete', {
+      description: 'Number of IdP connections delete requests',
+    }),
+
+    oauthAuthorize: meter.createCounter('jackson.oauth.authorize', {
+      description: 'Number of oauth authorize requests',
+    }),
+
+    oauthToken: meter.createCounter('jackson.oauth.token', {
+      description: 'Number of oauth token requests',
+    }),
+
+    oauthUserInfo: meter.createCounter('jackson.oauth.userinfo', {
+      description: 'Number of oauth user info requests',
+    }),
+  };
+};
+
+export { increment, init };
