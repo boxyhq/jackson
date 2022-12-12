affa46fe6d818246183ed8f17e07f39d27d69b5f
Aswin V <vaswin91@gmail.com>
2022-12-13 00:59:33 +0530

    Minor tweak comment

--- sdk/ui/react/src/utils/htmlIdGenerator.ts
+++ sdk/ui/react/src/utils/htmlIdGenerator.ts
@@ -1,7 +1,7 @@
 /**
  * Util function that helps to generate unique HTML ids that are namespaced by
  * the prefix and element type
- * @param prefix Pass anything that needs to be prefixed
+ * @param prefix Pass anything that needs to be prefixed to the id
  * @param elementType Pass the HTML element type here (e.g., input)
  * @returns {string} Id that is gauranteed to be unique and suitable for html id attributes across various components
  */
