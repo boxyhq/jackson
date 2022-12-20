14dc968c28c988c6316a4b94e676bbba9f7425fa
Kiran <kiran@boxyhq.com>
2022-12-20 21:45:46 +0530

    wip

---
+++ pages/api/admin/directory-sync/[directoryId]/users/index.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import jackson from '@lib/jackson';
+import { checkSession } from '@lib/middleware';
+
+export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      return handleGET(req, res);
+    default:
+      res.setHeader('Allow', 'GET');
+      res.status(405).json({ error: { message: `Method ${method} Not Allowed` } });
+  }
+};
+
+const handleGET = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { directorySyncController } = await jackson();
+
+  const { directoryId } = req.query as { directoryId: string };
+
+  const { data, error } = await directorySyncController.directories.get(directoryId);
+
+  if (data) {
+    return res.status(200).json({ data });
+  }
+
+  if (error) {
+    return res.status(error.code).json({ error });
+  }
+};
+
+export default checkSession(handler);
