8e558a529f4bcdea19d5d09f084eae30c85fcafc
Deepak Prabhakara <deepak@boxyhq.com>
2022-12-21 21:28:56 +0000

    switched to ButtonIcon

--- pages/admin/retraced/projects/index.tsx
+++ pages/admin/retraced/projects/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { InformationCircleIcon, DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { DocumentMagnifyingGlassIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import EmptyState from '@components/EmptyState';
 import { useProjects } from '@lib/ui/retraced';
 import Loading from '@components/Loading';
 import ErrorMessage from '@components/Error';
+import { IconButton } from '@components/IconButton';
+import { useTranslation } from 'next-i18next';
+import router from 'next/router';
 
 const ProjectList: NextPage = () => {
+  const { t } = useTranslation('common');
+
   const { projects, isError, isLoading } = useProjects();
 
   if (isLoading) {
@@ -61,14 +66,24 @@ const ProjectList: NextPage = () => {
                       {project.created}
                     </td>
                     <td className='px-6 py-3'>
-                      <div className='flex gap-2'>
-                        <Link href={`/admin/retraced/projects/${project.id}`} className='link-primary'>
-                          <InformationCircleIcon className='h-5 w-5 text-secondary' />
-                        </Link>
-                        <Link href={`/admin/retraced/projects/${project.id}/events`} className='link-primary'>
-                          <DocumentMagnifyingGlassIcon className='h-5 w-5 text-secondary' />
-                        </Link>
-                      </div>
+                      <span className='inline-flex items-baseline'>
+                        <IconButton
+                          tooltip={t('configuration')}
+                          Icon={WrenchScrewdriverIcon}
+                          className='mr-3 hover:text-green-400'
+                          onClick={() => {
+                            router.push(`/admin/retraced/projects/${project.id}`);
+                          }}
+                        />
+                        <IconButton
+                          tooltip={t('view_events')}
+                          Icon={DocumentMagnifyingGlassIcon}
+                          className='mr-3 hover:text-green-400'
+                          onClick={() => {
+                            router.push(`/admin/retraced/projects/${project.id}/events`);
+                          }}
+                        />
+                      </span>
                     </td>
                   </tr>
                 ))}
