e378b9eddba9d9ec683739e104517bde989cc238
Kiran <kiran@boxyhq.com>
2022-12-26 20:26:57 +0530

    Tweaks

--- components/CreateSetupLink.tsx
+++ components/CreateSetupLink.tsx
@@ -153,7 +153,7 @@ const CreateSetupLink = (props: { service: 'sso' | 'dsync' }) => {
                 })}
           </h2>
           <div className='form-control'>
-            <InputWithCopyButton text={url} label='Setup Link URL' />
+            <InputWithCopyButton text={url} label={t('setup_link_url')} />
           </div>
           <div className='mt-5 flex'>
             <ButtonPrimary
