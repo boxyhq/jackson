ddc5709b85c4c0522883bf3988cc9e48331067a0
Aswin V <vaswin91@gmail.com>
2023-01-03 13:50:10 +0530

    Tweak edit page for settings view

--- pages/admin/settings/sso-connection/edit/[id].tsx
+++ pages/admin/settings/sso-connection/edit/[id].tsx
@@ -3,34 +3,39 @@ import useSWR from 'swr';
 import { useRouter } from 'next/router';
 
 import { fetcher } from '@lib/ui/utils';
-import Edit from '@components/connection/Edit';
+import EditConnection from '@components/connection/EditConnection';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Loading from '@components/Loading';
+import { errorToast } from '@components/Toaster';
+import type { ApiError, ApiSuccess } from 'types';
+import type { OIDCSSORecord, SAMLSSORecord } from '@boxyhq/saml-jackson';
 
-const EditConnection: NextPage = () => {
+const EditSSOConnection: NextPage = () => {
   const router = useRouter();
 
-  const { id } = router.query;
+  const { id } = router.query as { id: string };
 
-  const { data: connection, error } = useSWR(id ? `/api/admin/connections/${id}` : null, fetcher, {
-    revalidateOnFocus: false,
-  });
+  const { data, error } = useSWR<ApiSuccess<SAMLSSORecord | OIDCSSORecord>, ApiError>(
+    id ? `/api/admin/connections/${id}` : null,
+    fetcher,
+    {
+      revalidateOnFocus: false,
+    }
+  );
 
-  if (error) {
-    return (
-      <div className='rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700'>
-        {error.info ? JSON.stringify(error.info) : error.status}
-      </div>
-    );
+  if (!data && !error) {
+    return <Loading />;
   }
 
-  if (!connection) {
+  if (error) {
+    errorToast(error.message);
     return null;
   }
 
-  return <Edit connection={connection} selfSSOSetup={true} />;
+  return <EditConnection connection={data?.data} isSettingsView />;
 };
 
-export async function getServerSideProps({ locale }: GetServerSidePropsContext) {
+export async function getStaticProps({ locale }: GetServerSidePropsContext) {
   return {
     props: {
       ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
@@ -38,4 +43,4 @@ export async function getServerSideProps({ locale }: GetServerSidePropsContext) 
   };
 }
 
-export default EditConnection;
+export default EditSSOConnection;
