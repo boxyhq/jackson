40f0cd1750c7370023b245e44bec2082fddcb18a
Aswin V <vaswin91@gmail.com>
2023-01-03 13:49:13 +0530

    Take in admin SSO defaults from env

--- pages/admin/settings/sso-connection/new.tsx
+++ pages/admin/settings/sso-connection/new.tsx
@@ -1,15 +1,28 @@
 import type { GetServerSidePropsContext, NextPage } from 'next';
-import Add from '@components/connection/Add';
+import CreateConnection from '@components/connection/CreateConnection';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import type { AdminSSODefaults } from '@components/connection/utils';
+import { jacksonOptions } from '@lib/env';
 
-const NewSSOConnection: NextPage = () => {
-  return <Add showBackButton={false} titleText={'Create Admin Panel SSO Connection'} selfSSOSetup={true} />;
+type Props = {
+  adminSSODefaults: AdminSSODefaults;
+};
+
+const NewSSOConnection: NextPage<Props> = ({ adminSSODefaults }) => {
+  return <CreateConnection isSettingsView adminSSODefaults={adminSSODefaults} />;
 };
 
 export async function getStaticProps({ locale }: GetServerSidePropsContext) {
+  const { externalUrl } = jacksonOptions;
   return {
     props: {
       ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
+      adminSSODefaults: {
+        tenant: process.env.ADMIN_PORTAL_TENANT,
+        product: process.env.ADMIN_PORTAL_PRODUCT,
+        redirectUrl: externalUrl,
+        defaultRedirectUrl: `${externalUrl}/api/auth/callback/boxyhq-saml`,
+      },
     },
   };
 }
