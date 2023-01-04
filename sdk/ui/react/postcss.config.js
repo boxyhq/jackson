d7f4340efe7f565a2c619ce680b8a58dc22aaf22
Aswin V <vaswin91@gmail.com>
2023-01-04 12:07:41 +0530

    Revert removal of autoprefixer

--- sdk/ui/react/postcss.config.js
+++ sdk/ui/react/postcss.config.js
@@ -1,3 +1,4 @@
+// Had to create this file and install autoprefixer dev dependency to fix vite error: "[plugin:vite:css] [postcss] Cannot read properties of undefined (reading 'config')"
 const postcss = {
   plugins: {
     autoprefixer: {},
