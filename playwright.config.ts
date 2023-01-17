ba69bfe2368b0b1c95c623fc60d227e5d7ff1a81
Aswin V <vaswin91@gmail.com>
2023-01-18 01:13:28 +0530

    - Use production build for local testing  - Set NODE_ENV to pick up .env.test.local

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,13 @@ const config: PlaywrightTestConfig = {
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: process.env.CI ? 'npm run start' : 'npm run postgres',
+    command: process.env.CI ? 'npm run start' : 'npm run build && npm run start',
     port: 5225,
     timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
+    env: {
+      NODE_ENV: 'test',
+    },
   },
 
   use: {
