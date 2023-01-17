aa5a9e97d430a67769179f5fdb9f4239d0a2122e
Aswin V <vaswin91@gmail.com>
2023-01-18 01:19:54 +0530

    Fix tests

--- e2e/ui/admin_portal/settings.spec.ts
+++ e2e/ui/admin_portal/settings.spec.ts
@@ -1,8 +1,7 @@
 import { expect, test } from '@playwright/test';
 
-test.only('Add SSO connection to mocksaml.com for admin portal login', async ({ page }) => {
+test('Add SSO connection to mocksaml.com for admin portal login', async ({ page }) => {
   await page.goto('/admin/settings');
-
   // Find the add connection button and click on it
   await page.getByTestId('create-connection').click();
   // Enter the metadata url for mocksaml.com
@@ -12,13 +11,14 @@ test.only('Add SSO connection to mocksaml.com for admin portal login', async ({ 
   // Add the connection
   await page.getByTestId('submit-form-create-sso').click();
   // Check if new entry appears in connection list page
-  await page.waitForURL('/admin/settings');
   await expect(page.getByText('saml.example.com')).toBeVisible();
-  // Logout to test login using the above connection
-  await page.getByTestId('logout');
 });
 
-test.only('Login via SSO connection to mocksaml.com added previously', async ({ page }) => {
+test('Login via SSO connection to mocksaml.com added previously', async ({ page }) => {
+  await page.goto('/');
+  await page.getByTestId('user-avatar').click();
+  // Logout to test login using the above connection
+  await page.getByTestId('logout').click();
   await page.getByTestId('sso-login-button').click();
   await page.waitForURL('https://mocksaml.com');
 });
