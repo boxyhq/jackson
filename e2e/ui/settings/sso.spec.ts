b6669ecac0081d67f77a9bd63345850e23050fcd
Aswin V <vaswin91@gmail.com>
2023-01-19 23:00:27 +0530

    Final changes

--- e2e/ui/settings/sso.spec.ts
+++ e2e/ui/settings/sso.spec.ts
@@ -1,30 +1,53 @@
 import { expect, test } from '@playwright/test';
 
+const TEST_SSO_CONNECTION_NAME = 'pw_admin_portal_sso';
+const MOCKSAML_ORIGIN = 'https://mocksaml.com';
+const MOCKSAML_METADATA_URL = `${MOCKSAML_ORIGIN}/api/saml/metadata`;
+const MOCKSAML_SIGNIN_BUTTON_NAME = 'Sign In';
+
 test.describe('Admin Portal SSO', () => {
   test('should be able to add SSO connection to mocksaml.com', async ({ page }) => {
     await page.goto('/admin/settings');
-    // Find the add connection button and click on it
+    // Find the new connection button and click on it
     await page.getByTestId('create-connection').click();
-    // Enter the metadata url for mocksaml.com
+    // Fill the name for the connection
+    const nameInput = page.locator('#name');
+    await nameInput.fill(TEST_SSO_CONNECTION_NAME);
+    // Enter the metadata url for mocksaml.com in the form
     const metadataUrlInput = page.locator('#metadataUrl');
-    await metadataUrlInput.scrollIntoViewIfNeeded();
-    await metadataUrlInput.fill('https://mocksaml.com/api/saml/metadata');
+    await metadataUrlInput.fill(MOCKSAML_METADATA_URL);
+    // submit the form
     await page.getByTestId('submit-form-create-sso').click();
-    await expect(page.getByText('saml.example.com')).toBeVisible();
+    // check if the added connection appears in the connection list
+    await expect(page.getByText(TEST_SSO_CONNECTION_NAME)).toBeVisible();
   });
 
   test('should be able to login via mocksaml.com SSO', async ({ page, baseURL }) => {
     const userAvatarLocator = page.getByTestId('user-avatar');
-
+    // Logout from the magic link authentication
     await page.goto('/');
     await userAvatarLocator.click();
-    // Logout to test login using the above connection
     await page.getByTestId('logout').click();
+    // Click on login with sso button
     await page.getByTestId('sso-login-button').click();
-    await page.waitForURL((url) => url.origin === 'https://mocksaml.com');
+    // Perform sign in at mocksaml
+    await page.waitForURL((url) => url.origin === MOCKSAML_ORIGIN);
     await page.getByPlaceholder('jackson').fill('bob');
-    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.getByRole('button', { name: MOCKSAML_SIGNIN_BUTTON_NAME }).click();
+    // Wait for browser to redirect back to admin portal
     await page.waitForURL((url) => url.origin === baseURL);
+    // assert login state
     await expect(userAvatarLocator).toBeVisible();
+  });
+
+  test('delete the SSO connection', async ({ page }) => {
+    await page.goto('/admin/settings');
+    // select the row of the connection list table, then locate the edit button
+    const editButton = page.getByText(TEST_SSO_CONNECTION_NAME).locator('..').getByTestId('edit');
+    await editButton.click();
+    await page.getByTestId('delete-connection').click();
+    await page.getByTestId('confirm-delete').click();
+    // check that the SSO connection is absent in the connection list
+    await expect(page.getByText('pw_admin_portal_sso')).not.toBeVisible();
   });
 });
