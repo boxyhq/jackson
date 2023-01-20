305ff93cbb4d00f5601d3a053469a07236a23329
Aswin V <vaswin91@gmail.com>
2023-01-20 16:07:01 +0530

    E2E tests for admin portal SSO (#846)
    * Sync lock file
    
    * Add admin portal tests
    
    * Use data-testid to enable `page.getByTestId()`
    
    * Support data-testid for inner elements
    
    * Pass data-testid to sso login button
    
    * Remove env-cmd
    
    * Update folder structure
    
    * Add data-testid
    
    * - Use production build for local testing
     - Set NODE_ENV to pick up .env.test.local
    
    * Add test-id for logout dropdown activation
    
    * Remove test; superseded with new tests
    
    * Fix tests
    
    * Wait for navigation to complete
    
    * [Failing ci test try fix] Pass url for waiting
    
    * [Failing ci test try fix] skip assertion
    
    * [Fix failing test] use predicate to match origin
    
    * Add back the visibility check
    
    * Fix query param
    
    * Tweak test code
    
    * Update return type of callback
    
    * Group tests and reorganise folders
    
    * Add actions to login and assert by locator
    
    * Support html attribute passing
    
    * Set data-testid on edit button
    
    * Set data-testids
    
    * Final changes
    
    * Tweak

--- components/connection/ConnectionList.tsx
+++ components/connection/ConnectionList.tsx
@@ -72,14 +72,14 @@ const ConnectionList = ({
           {t(isSettingsView ? 'admin_portal_sso' : 'enterprise_sso')}
         </h2>
         <div className='flex gap-2'>
-          <LinkPrimary Icon={PlusIcon} href={createConnectionUrl} data-test-id='create-connection'>
+          <LinkPrimary Icon={PlusIcon} href={createConnectionUrl} data-testid='create-connection'>
             {t('new_connection')}
           </LinkPrimary>
           {!setupLinkToken && !isSettingsView && (
             <LinkPrimary
               Icon={LinkIcon}
               href='/admin/sso-connection/setup-link/new'
-              data-test-id='create-setup-link'>
+              data-testid='create-setup-link'>
               {t('new_setup_link')}
             </LinkPrimary>
           )}
@@ -165,6 +165,7 @@ const ConnectionList = ({
                             tooltip={t('edit')}
                             Icon={PencilIcon}
                             className='hover:text-green-400'
+                            data-testid='edit'
                             onClick={() => {
                               router.push(
                                 setupLinkToken
--- e2e/admin.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-
-test('MAGIC_LINK in globalSetup should log me in', async ({ page }) => {
-  await page.goto('/admin/sso-connection');
-
-  // Find the button and click on it
-  await page.locator('data-test-id=create-connection').click();
-});
