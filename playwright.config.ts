77488d1639ef00b34e7283672666ebe83b6a00a8
Aswin V <vaswin91@gmail.com>
2023-02-04 14:11:15 +0530

    Try with cmd line option for webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,9 @@ const config: PlaywrightTestConfig = {
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: process.env.CI ? 'npm run start' : 'npm run build && npm run start',
+    command: process.env.CI
+      ? 'npm run start -- --dns-result-order=ipv4first'
+      : 'npm run build && npm run start',
     port: 5225,
     timeout: 60 * 1000,
     reuseExistingServer: !process.env.CI,
