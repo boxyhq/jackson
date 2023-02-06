5d411b5252e06cf98c7fc4823fbb8b664330e18f
Aswin V <vaswin91@gmail.com>
2023-02-06 15:46:02 +0530

    SAML Idp login using credentials provider

---
+++ pages/admin/auth/idp-login.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/react';
+
+export default function Page() {
+  const router = useRouter();
+
+  const { isReady, query } = router;
+
+  useEffect(() => {
+    if (!isReady) return;
+
+    signIn('boxyhq-saml-idplogin', { code: query?.code });
+  }, [isReady, query?.code]);
+
+  return null;
+}
