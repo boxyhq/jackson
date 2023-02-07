887750d068e596168ec6e3a1839b5ad5d02ee1d9
Kiran K <kiran@boxyhq.com>
2023-02-07 17:55:28 +0530

    Email password login (#883)
    * display toast and adjust the width of the content
    
    * Login with email and password
    
    * Find the admin credentials that match the email and password
    
    * tweaks
    
    * replace <a> with <button>
    
    * renamed env var, added docs to .env.example
    
    * updates based on the feedback
    
    * hide/show the login methods based on whether they have been enabled or not.
    
    * tweaked text
    
    ---------
    
    Co-authored-by: Deepak Prabhakara <deepak@boxyhq.com>

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import BoxyHQSAMLProvider from 'next-auth/providers/boxyhq-saml';
 import { validateEmailWithACL } from '@lib/utils';
 import { jacksonOptions as env } from '@lib/env';
 import { sessionName } from '@lib/constants';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export default NextAuth({
   theme: {
@@ -32,6 +33,49 @@ export default NextAuth({
       },
       from: process.env.SMTP_FROM,
     }),
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        const { email, password } = credentials;
+
+        if (!email || !password) {
+          return null;
+        }
+
+        const adminCredentials = process.env.NEXTAUTH_ADMIN_CREDENTIALS;
+
+        if (!adminCredentials) {
+          throw Error(
+            'No admin credentials found. Please set NEXTAUTH_ADMIN_CREDENTIALS in your environment variables'
+          );
+        }
+
+        // Find the admin credentials that match the email and password
+        const adminCredentialsMatch = adminCredentials.split(',').find((credential) => {
+          const [adminEmail, adminPassword] = credential.split(':');
+
+          return adminEmail === email && adminPassword === password;
+        });
+
+        // No match found
+        if (!adminCredentialsMatch) {
+          throw Error('Invalid email or password provided.');
+        }
+
+        return {
+          id: Buffer.from(email).toString('base64'),
+          name: email.split('@')[0],
+          email,
+        };
+      },
+    }),
   ],
   session: {
     strategy: 'jwt',
@@ -56,7 +100,13 @@ export default NextAuth({
       if (!user.email) {
         return false;
       }
-      return account?.provider === 'boxyhq-saml' ? true : validateEmailWithACL(user.email);
+
+      // Bypass ACL for credentials and boxyhq-saml
+      if (account?.provider === 'credentials' || account?.provider === 'boxyhq-saml') {
+        return true;
+      }
+
+      return validateEmailWithACL(user.email);
     },
   },
   pages: {
