8bd8ca00950f9a7e5f4628ef36d8346d6201b31a
Aswin V <vaswin91@gmail.com>
2023-02-16 01:04:21 +0530

    Skeleton - WIP

--- npm/src/ee/saml-tracer/index.ts
+++ npm/src/ee/saml-tracer/index.ts
@@ -1,3 +1,21 @@
-class SAMLTracer {}
+import { Storable } from '../../typings';
+import { randomUUID } from 'crypto';
+
+const TTL_1_WEEK = 604800;
+class SAMLTracer {
+  tracerStore: Storable;
+
+  constructor({ db }) {
+    this.tracerStore = db.store('saml:tracer', TTL_1_WEEK);
+  }
+
+  async saveTrace(payload) {
+    this.tracerStore.put(randomUUID(), payload);
+  }
+
+  async getTrace() {
+    this.tracerStore.getAll();
+  }
+}
 
 export default SAMLTracer;
