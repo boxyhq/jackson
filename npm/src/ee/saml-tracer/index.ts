4cb490cc6e1dcc70d76b64b36bcb87c8393d82ca
Aswin V <vaswin91@gmail.com>
2023-02-17 01:02:11 +0530

    Implement `cleanUpStaleTraces`

--- npm/src/ee/saml-tracer/index.ts
+++ npm/src/ee/saml-tracer/index.ts
@@ -3,7 +3,8 @@ import { randomUUID } from 'crypto';
 import { IndexNames } from '../../controller/utils';
 import { keyFromParts } from '../../db/utils';
 
-// const TTL_1_WEEK = 604800;
+const MILLISECONDS_1_WEEK = 604800000;
+const INTERVAL_1_DAY_MS = 86400000;
 
 type Trace = {
   traceId: string;
@@ -22,6 +23,7 @@ class SAMLTracer {
 
   constructor({ db }) {
     this.tracerStore = db.store('saml:tracer');
+    this.cleanUpStaleTraces();
   }
 
   public async saveTrace(payload: Omit<Trace, 'traceId'>) {
@@ -40,6 +42,23 @@ class SAMLTracer {
 
   public async getAllTraces(pageOffset?: number, pageLimit?: number): Promise<Trace[]> {
     return (await this.tracerStore.getAll(pageOffset, pageLimit)) as Trace[];
+  }
+
+  private cleanUpStaleTraces() {
+    setInterval(async () => {
+      const traces: Trace[] = [];
+      for (let pageOffset = 0; ; pageOffset++) {
+        const page = await this.getAllTraces(pageOffset, 50);
+        if (page.length === 0) {
+          break;
+        }
+        traces.concat(page.filter(({ timestamp }) => Date.now() - timestamp > MILLISECONDS_1_WEEK));
+      }
+
+      for (let i = 0; i < traces.length; i++) {
+        this.tracerStore.delete(traces[i].traceId);
+      }
+    }, INTERVAL_1_DAY_MS);
   }
 }
 
