77c80ef76873d6878fef0048e3ea4ad274cb743f
Aswin V <vaswin91@gmail.com>
2023-02-24 12:23:49 +0530

    Placeholder for parsing rawResponse

--- pages/api/admin/saml-tracer/[traceId]/index.ts
+++ pages/api/admin/saml-tracer/[traceId]/index.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import jackson from '@lib/jackson';
-import type { SAMLTracerInstance } from '@boxyhq/saml-jackson';
+import type { SAMLTrace, SAMLTracerInstance } from '@boxyhq/saml-jackson';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { samlTracer } = await jackson();
@@ -27,6 +27,11 @@ const handleGET = async (req: NextApiRequest, res: NextApiResponse, samlTracer: 
   const { traceId } = req.query as { traceId: string };
 
   const trace = await samlTracer.getByTraceId(traceId);
+  const context = trace.context as SAMLTrace['context'];
+
+  if ('samlResponse' in context) {
+    // parse raw response and attach it to returned trace
+  }
 
   if (!trace) {
     return res.status(404).json({ error: { message: 'Trace not found.' } });
