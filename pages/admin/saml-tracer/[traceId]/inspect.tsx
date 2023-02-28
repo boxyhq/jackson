4f0a7e28e3b4f0ae04241062c54b85b4da217f6f
Aswin V <vaswin91@gmail.com>
2023-02-28 15:36:10 +0530

    Refactor and style changes, display more context

--- pages/admin/saml-tracer/[traceId]/inspect.tsx
+++ pages/admin/saml-tracer/[traceId]/inspect.tsx
@@ -1,7 +1,7 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { Trace } from '@boxyhq/saml-jackson';
+import type { Trace } from '@boxyhq/saml-jackson';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialOceanic } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import useSWR from 'swr';
@@ -9,8 +9,19 @@ import { ApiSuccess, ApiError } from 'types';
 import { fetcher } from '@lib/ui/utils';
 import { errorToast } from '@components/Toaster';
 import Loading from '@components/Loading';
+import { useTranslation } from 'react-i18next';
+import { LinkBack } from '@components/LinkBack';
+
+const DescriptionListItem = ({ term, value }: { term: string; value: string | JSX.Element }) => (
+  <div className='px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+    <dt className='text-sm font-medium text-gray-500'>{term}</dt>
+    <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>{value}</dd>
+  </div>
+);
 
 const SAMLTraceInspector: NextPage = () => {
+  const { t } = useTranslation('common');
+
   const router = useRouter();
 
   const { traceId } = router.query as { traceId: string };
@@ -35,38 +46,52 @@ const SAMLTraceInspector: NextPage = () => {
   if (!data) return null;
 
   return (
-    <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
-      <div className='px-4 py-5 sm:px-6'>
-        <h3 className='text-base font-semibold leading-6 text-gray-900'>Trace details</h3>
-        <p className='mt-1 max-w-2xl text-sm text-gray-500'>Inspect the trace</p>
+    <>
+      <LinkBack onClick={() => router.back()} />
+      <div className='mt-5 overflow-hidden bg-white shadow sm:rounded-lg'>
+        <div className='px-4 py-5 sm:px-6'>
+          <h3 className='text-base font-semibold leading-6 text-gray-900'>Trace details</h3>
+          <p className='mt-1 flex max-w-2xl gap-6 text-sm text-gray-500'>
+            <div>
+              <span className='font-medium text-gray-500'>Trace ID:</span>
+              <span className='ml-2 font-bold text-gray-700'> {traceId}</span>
+            </div>
+            <div>
+              <span className='font-medium text-gray-500'>{t('assertion_type')}:</span>
+              <span className='ml-2 font-bold text-gray-700'>
+                {data.data.context?.samlResponse ? 'Response' : 'Request'}
+              </span>
+            </div>
+          </p>
+        </div>
+        <div className='border-t border-gray-200'>
+          <dl>
+            <DescriptionListItem term='Timestamp' value={new Date(data.data.timestamp).toLocaleString()} />
+            <DescriptionListItem term='Error' value={data.data.error} />
+            {typeof data.data.context.tenant === 'string' && (
+              <DescriptionListItem term='Tenant' value={data.data.context.tenant} />
+            )}
+            {typeof data.data.context.product === 'string' && (
+              <DescriptionListItem term='Product' value={data.data.context.product} />
+            )}
+            {typeof data.data.context.issuer === 'string' && (
+              <DescriptionListItem term='Product' value={data.data.context.issuer} />
+            )}
+            {typeof data.data.context.profile === 'string' && (
+              <DescriptionListItem term='Product' value={data.data.context.profile} />
+            )}
+            <DescriptionListItem
+              term='Raw response'
+              value={
+                <SyntaxHighlighter language='xml' style={materialOceanic}>
+                  {data?.data.context.samlResponse}
+                </SyntaxHighlighter>
+              }
+            />
+          </dl>
+        </div>
       </div>
-      <div className='border-t border-gray-200'>
-        <dl>
-          <div className='px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-            <dt className='text-sm font-medium text-gray-500'>Trace ID</dt>
-            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>{traceId}</dd>
-          </div>
-          <div className='px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-            <dt className='text-sm font-medium text-gray-500'>Timestamp</dt>
-            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
-              {new Date(data.data.timestamp).toLocaleString()}
-            </dd>
-          </div>
-          <div className='px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-            <dt className='text-sm font-medium text-gray-500'>Error</dt>
-            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>{data.data.error}</dd>
-          </div>
-          <div className='px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-            <dt className='text-sm font-medium text-gray-500'>Raw response</dt>
-            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0'>
-              <SyntaxHighlighter language='xml' style={materialOceanic}>
-                {data?.data.context.samlResponse}
-              </SyntaxHighlighter>
-            </dd>
-          </div>
-        </dl>
-      </div>
-    </div>
+    </>
   );
 };
 
