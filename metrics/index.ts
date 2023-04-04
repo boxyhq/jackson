e4d0f3703c0fe799421388acc269a4d7f1431eb8
Aswin V <vaswin91@gmail.com>
2023-04-04 23:34:27 +0545

    Rename folder and add npm dependencies

--- metrics/index.ts
+++ metrics/index.ts
@@ -1,2 +1,2 @@
 export { initializeOtel } from './init';
-export { incrementOtelCounter, observeOtelGauge, recordOtelHistogram } from './instrumentation/instruments';
+export { incrementOtelCounter, observeOtelGauge, recordOtelHistogram } from './src/instruments';
