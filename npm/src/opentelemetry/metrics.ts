27e5c59085be96df91b49a250285d1263b009741
Aswin V <vaswin91@gmail.com>
2023-04-06 14:16:08 +0545

    Swap creation of instruments with constants

--- npm/src/opentelemetry/metrics.ts
+++ npm/src/opentelemetry/metrics.ts
@@ -1,43 +1,28 @@
-import { metrics } from '@opentelemetry/api';
+export const METER = 'jackson';
 
-let meter = metrics.getMeterProvider().getMeter('jackson');
-let counters;
-
-const increment = (action: string) => {
-  const counter = counters[action];
-  if (counter) {
-    counter.add(1);
-  }
+export const COUNTERS = {
+  createConnection: {
+    metricName: 'jackson.connection.create',
+    metricDescription: 'Number of IdP connection create requests',
+  },
+  getConnections: {
+    metricName: 'jackson.connection.get',
+    metricDescription: 'Number of IdP connection get requests',
+  },
+  deleteConnections: {
+    metricName: 'jackson.connection.delete',
+    metricDescription: 'Number of IdP connections delete requests',
+  },
+  oauthAuthorize: {
+    metricName: 'jackson.oauth.authorize',
+    metricDescription: 'Number of oauth authorize requests',
+  },
+  oauthToken: {
+    metricName: 'jackson.oauth.token',
+    metricDescription: 'Number of oauth token requests',
+  },
+  oauthUserInfo: {
+    metricName: 'jackson.oauth.userinfo',
+    metricDescription: 'Number of oauth user info requests',
+  },
 };
-
-const init = () => {
-  meter = metrics.getMeterProvider().getMeter('jackson');
-
-  counters = {
-    createConnection: meter.createCounter('jackson.connection.create', {
-      description: 'Number of IdP connection create requests',
-    }),
-
-    getConnections: meter.createCounter('jackson.connection.get', {
-      description: 'Number of IdP connections get requests',
-    }),
-
-    deleteConnections: meter.createCounter('jackson.connection.delete', {
-      description: 'Number of IdP connections delete requests',
-    }),
-
-    oauthAuthorize: meter.createCounter('jackson.oauth.authorize', {
-      description: 'Number of oauth authorize requests',
-    }),
-
-    oauthToken: meter.createCounter('jackson.oauth.token', {
-      description: 'Number of oauth token requests',
-    }),
-
-    oauthUserInfo: meter.createCounter('jackson.oauth.userinfo', {
-      description: 'Number of oauth user info requests',
-    }),
-  };
-};
-
-export { increment, init };
