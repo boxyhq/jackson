8c88a4431bf93a5a17a641fc6199f5045f61d8af
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-18 12:08:29 +0530

    Init files for vue2 setup

---
+++ sdk/ui/vue2/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1 class="text-5xl font-bold px-5 py-2">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ sdk/ui/vue2/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import './style.css'
+import App from './App.vue'
+
+new Vue({
+    render: (h) => h(App),
+  }).$mount('#app');
