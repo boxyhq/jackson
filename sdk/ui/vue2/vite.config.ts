e84963a3a35767a1fd9e238d1b49ba54ff689b56
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-19 12:04:33 +0530

    Updating vite config

---
+++ sdk/ui/vue2/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // use vite library mode to build the package
+  // https://vitejs.dev/guide/build.html#library-mode
+  build:{
+    lib:{
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'BoxyHQUI',
+      // the proper extensions will be added
+      fileName: '@boxyhq/vue2-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      }
+    }
+  },
+  plugins: [vue()],
+})
