50774959791efffd5de4f1bac2fbd75d7857b196
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-24 11:56:22 +0530

    Import and test Login component file in home page

--- sdk/ui/svelte/src/routes/+page.svelte
+++ sdk/ui/svelte/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Login from "./Login.svelte";
+
+    function onSubmit() {
+    console.log('SSO flow intialized');
+   }
+</script>
+
+<main>
+    <Login ssoIdentifier={'some-sso-identifier'} onSubmit={onSubmit}/>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    <Login onSubmit={onSubmit} inputLabel={'Team domain*'} buttonText={'Login with SSO'} placeholder={'contos@boxyhq.com'} />
+</main>
