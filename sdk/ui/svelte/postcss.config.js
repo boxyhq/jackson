63470057dbc095eda08378a64db96105f3421107
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-24 11:50:41 +0530

    Add postcss config file to fix vite error:plugin:vite:css] [postcss] Cannot read properties of undefined (reading config)

---
+++ sdk/ui/svelte/postcss.config.js
@@ -0,0 +1,7 @@
+// Had to create this file and install autoprefixer dev dependency to fix vite error: "[plugin:vite:css] [postcss] Cannot read properties of undefined (reading 'config')"
+const postcss = {
+    plugins: {
+      autoprefixer: {},
+    },
+  };
+export default postcss;
