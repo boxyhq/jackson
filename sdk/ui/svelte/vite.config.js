b95f272e7be5ba623bb10e956d0f714fbea87960
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-24 12:43:02 +0530

    Update vite config

--- sdk/ui/svelte/vite.config.js
+++ sdk/ui/svelte/vite.config.js
@@ -1,6 +1,30 @@
+import { resolve } from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
+  build: {
+    // use vite library mode to build the package
+    // https://vitejs.dev/guide/build.html#library-mode
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'BoxyHQUI',
+      // the proper extensions will be added
+      fileName: '@boxyhq/svelte-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['svelte'],
+      output: {
+        globals: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          sveltekit: 'sveltekit',
+        },
+      },
+    },
+  },
+  plugins: [sveltekit()],
 });
