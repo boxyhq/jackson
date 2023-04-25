bb4a9bfd414d0e5a46cb0edb491cf74bc3e3f79b
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-25 12:59:25 +0530

    Update vite config

--- sdk/ui/svelte/vite.config.js
+++ sdk/ui/svelte/vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   build: {
     // use vite library mode to build the package
@@ -16,12 +16,12 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['svelte'],
+      external: ['svelte'],   
       output: {
         globals: {
           // Provide global variables to use in the UMD build
           // for externalized deps
-          sveltekit: 'sveltekit',
+          svelte: 'Svelte',
         },
       },
     },
