fe3bde42b43e86b481f9b5a04a3e4579852f7064
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-25 13:01:54 +0530

    Update vite config

--- sdk/ui/svelte/docs/vite.config.js
+++ sdk/ui/svelte/docs/vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import * as path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  resolve: {
+    alias: {
+      '@boxyhq/svelte-ui': path.join(__dirname, '../src'),
+    },
+  },
+  plugins: [sveltekit()],
 });
