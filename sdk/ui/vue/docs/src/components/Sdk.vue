d1a853e83f2845fa7bdd4dd64e3024dd903600f4
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-04-25 12:36:55 +0530

    Format and update vue documentation

--- sdk/ui/vue/docs/src/components/Sdk.vue
+++ sdk/ui/vue/docs/src/components/Sdk.vue
@@ -1,67 +1,73 @@
 <template>
+  <main class="hero-body-componentss">
     <div class="hero-body">
       <h1 class="welcome">@boxyhq/vue-ui</h1>
-      <p class="desciption">UI components from <span class="boxyhq-link"><a href="https://boxyhq.com/" target="_blank">BoxyHQ</a></span> for plug-and-play enterprise features.</p>
+      <p class="desciption">
+        UI components from <span class="boxyhq-link"><a href="https://boxyhq.com/" target="_blank">BoxyHQ</a></span> for
+        plug-and-play enterprise features.
+      </p>
       <h1 class="installtion">Installation</h1>
       <p><code class="installtion-instructions">npm install @boxyhq/vue-ui --save</code></p>
-      <h2 class="usage">Usage</h2> 
+      <h2 class="usage">Usage</h2>
       <h3 class="sso-login-component">SSO Login Component</h3>
       <p class="sso-ways">There are mainly 2 ways of using the SSO Login Component as outlined below:</p>
       <h4 class="value-preset">Preset value for <code class="sso-identifier">ssoIdentifier</code></h4>
-      <p>If a value is passed for <code class="sso-identifier-light">ssoIdentifier</code>, it would render a button that on click calls the passed-in handler (onSubmit) with the <code class="sso-identifier-light">ssoIdentifier</code> value. The handler can then initiate a redirect to the SSO service forwarding the value for ssoIdentifier.</p>
-    <pre><code class="language-js prism-code">
-import { Login as SSOLogin } from '@boxyhq/vue3-ui';
-
-const onSSOSubmit = async (ssoIdentifier: string) => {
-  // Below calls signIn from next-auth. Replace this with whatever auth lib that you are using.
-  await signIn('boxyhq-saml', undefined, { client_id: ssoIdentifier });
-}; 
-
+      <p>
+        If a value is passed for <code class="sso-identifier-light">ssoIdentifier</code>, it would render a button that
+        on click calls the passed-in handler (onSubmit) with the
+        <code class="sso-identifier-light">ssoIdentifier</code> value. The handler can then initiate a redirect to the
+        SSO service forwarding the value for ssoIdentifier.
+      </p>
+      <pre><code class="language-js prism-code">
 {{ ssoPresetCode }}
     </code></pre>
-    <h4 class="value-preset">Accept input from the user for <code class="sso-identifier">ssoIdentifier</code></h4>
-    <p>If a value is not passed for <code class="sso-identifier-light">ssoIdentifier</code>, it would render an input field for the user to enter the <code class="sso-identifier-light">ssoIdentifier</code> value. And then on submit, the value gets passed to the handler. The handler can then initiate a redirect to the SSO service forwarding the value for ssoIdentifier.</p>
-    <pre><code class="language-js">
-import { Login as SSOLogin } from '@boxyhq/vue3-ui';
-
-const onSSOSubmit = async (ssoIdentifier: string) => {
-  // Below calls signIn from next-auth. Replace this with whatever auth lib that you are using.
-  await signIn('boxyhq-saml', undefined, { client_id: ssoIdentifier });
-};
-
+      <h4 class="value-preset">Accept input from the user for <code class="sso-identifier">ssoIdentifier</code></h4>
+      <p>
+        If a value is not passed for <code class="sso-identifier-light">ssoIdentifier</code>, it would render an input
+        field for the user to enter the <code class="sso-identifier-light">ssoIdentifier</code> value. And then on
+        submit, the value gets passed to the handler. The handler can then initiate a redirect to the SSO service
+        forwarding the value for ssoIdentifier.
+      </p>
+      <pre><code class="language-js">
 {{accessInputCode}}
-    </code></pre>    
-    <h4 class="value-preset">Styling</h4>
-    <p>If the classNames prop is passed in, we can override the default styling for each inner element. In case an inner element is omitted from the classNames prop, default styles will be set for the element. For example, In the below snippet, all the inner elements are styled by passing in the classNames for each inner one.</p>
-    <pre><code class="language-js">{{ stylingCode }}</code></pre>
-    <p class="end-para">Styling via style attribute is also supported for each inner element.</p>
-  </div>
-  </template>
-  
-  <script>
+    </code></pre>
+      <h4 class="value-preset">Styling</h4>
+      <p>
+        If the classNames prop is passed in, we can override the default styling for each inner element. In case an
+        inner element is omitted from the classNames prop, default styles will be set for the element. For example, In
+        the below snippet, all the inner elements are styled by passing in the classNames for each inner one.
+      </p>
+      <pre><code class="language-js">{{ stylingCode }}</code></pre>
+      <p class="end-para">Styling via style attribute is also supported for each inner element.</p>
+    </div>
+  </main>
+</template>
 
+<script>
 import Prism from 'prismjs';
 import 'prismjs/themes/prism.css';
 import 'prismjs/components/prism-jsx';
 
-  export default {
-    name: "sdk",
-  
-    props: ["greeting"],
-  
-    data() {
-      return { name: "Rajiv",
-      ssoPresetCode:"<SSOLogin \n  buttonText='Login with SSO'\n  :ssoIdentifier=''tenant=' + {{tenant}} + '&product=' + {{product}}'\n  :onSubmit={{onSSOSubmit}}\n  :classNames='{\n    container: 'mt-2'\n    button: 'btn-primary btn-block btn rounded-md active:-scale-95',\n}'\n/>;", 
-      accessInputCode:"<SSOLogin \n  buttonText='Login with SSO'\n  onSubmit={{onSSOSubmit}}\n  :classNames='{\n    container: 'mt-2',\n    label: 'text-gray-400',\n    button: 'btn-primary btn-block btn rounded-md active:-scale-95',\n    input: 'input-bordered input mb-5 mt-2 w-full rounded-md',\n}'\n/>;",
-      stylingCode:"<SSOLogin\n  buttonText='Login with SSO'\n  onSubmit={{onSSOSubmit}}\n  :classNames='{\n    container: 'mt-2',\n    label: 'text-gray-400',\n    button: 'btn-primary btn-block btn rounded-md active:-scale-95',\n    input: 'input-bordered input mb-5 mt-2 w-full rounded-md'\n}'\n/>",
-    };
-    },
+export default {
+  name: 'sdk',
 
-    mounted() {
+  props: ['greeting'],
+
+  data() {
+    return {
+      ssoPresetCode:
+        "<template>\n  <SSOLogin \n    buttonText='Login with SSO'\n    :ssoIdentifier=''tenant=' + {{tenant}} + '&product=' + {{product}}'\n    :onSubmit={{onSSOSubmit}}\n    :classNames='{\n      container: 'mt-2'\n      button: 'btn-primary btn-block btn rounded-md active:-scale-95',\n     }'\n  />;\n</template>\n\n<script>\n  import { Login as SSOLogin } from '@boxyhq/vue3-ui';\n\n  const onSSOSubmit = async (ssoIdentifier: string) => {\n     // Below calls signIn from next-auth. Replace this with whatever auth lib that you are using.\n     await signIn('boxyhq-saml', undefined, { client_id: ssoIdentifier });\n  };\n< /script>",
+      accessInputCode:
+        "<template>\n  <SSOLogin \n    buttonText='Login with SSO'\n    onSubmit={{onSSOSubmit}}\n    :classNames='{\n      container: 'mt-2',\n      label: 'text-gray-400',\n      button: 'btn-primary btn-block btn rounded-md active:-scale-95',\n      input: 'input-bordered input mb-5 mt-2 w-full rounded-md',\n    }'\n  />;\n</template>\n\n<script>\n  import { Login as SSOLogin } from '@boxyhq/vue3-ui';\n\n  const onSSOSubmit = async (ssoIdentifier: string) => {\n  // Below calls signIn from next-auth. Replace this with whatever auth lib that you are using.\n  await signIn('boxyhq-saml', undefined, { client_id: ssoIdentifier });\n  };\n< /script>",
+      stylingCode:
+        "<template>\n  <SSOLogin\n    buttonText='Login with SSO'\n    onSubmit={{onSSOSubmit}}\n    :classNames='{\n    container: 'mt-2',\n    label: 'text-gray-400',\n    button: 'btn-primary btn-block btn rounded-md active:-scale-95',\n    input: 'input-bordered input mb-5 mt-2 w-full rounded-md'\n  }'\n</template>/>\n\n<script>\n  import { Login } from '@boxyhq/vue3-ui';\n< /script>",
+    };
+  },
+
+  mounted() {
     Prism.highlightAll();
-  }
-  };
-  </script>
-  
-  <style scoped>
-  </style>
+  },
+};
+</script>
+
+<style scoped></style>
--- sdk/ui/vue/docs/src/views/Components.vue
+++ sdk/ui/vue/docs/src/views/Components.vue
@@ -6,49 +6,77 @@ import { Login } from '@boxyhq/vue3-ui';
   <main class="hero-body-components">
     <div class="body-components">
       <div class="login-with-custom-styles">
-        <Login @onSubmit="onSubmitButton" :innerProps="{ input: { type: 'email' } }" inputLabel="Team Domain *" buttonText="Login with SSO" placeholder="contoso@boxyhq.com" :classNames="{ button: 'btn', input: 'inp'}" :styles="{ input: { borderColor: '#ebedf0' }, button: { padding: '.85rem' } }"/>
+        <Login
+          @onSubmit="onSubmitButton"
+          :innerProps="{ input: { type: 'email' } }"
+          inputLabel="Team Domain *"
+          buttonText="Login with SSO"
+          placeholder="contoso@boxyhq.com"
+          :classNames="{ button: 'btn', input: 'inp' }"
+          :styles="{ input: { borderColor: '#ebedf0' }, button: { padding: '.85rem' } }"
+        />
         <h2 class="hero-title">Login Component with custom styling</h2>
-        <h2 class="hero-description">Refer the code below to see the passed props. Also supported is the passing of style attribute for each inner element (Note that inline style will override other styles).</h2>
+        <h2 class="hero-description">
+          Refer the code below to see the passed props. Also supported is the passing of style attribute for each inner
+          element (Note that inline style will override other styles).
+        </h2>
         <button @click="toggleCodeSnippet('loginWithCustomStylingActive')" class="btn-toggle">Hide code</button>
         <div v-if="loginWithCustomStylingActive">
-        <pre><code class="language-js">
+          <pre><code class="language-js">
 {{ loginWithCustomStyling }}
         </code></pre>
         </div>
       </div>
       <div class="login-default-styles">
-      <Login :styles="{input: { border: '1px solid darkcyan' }}" @onSubmit="onSubmitButton" inputLabel="Team domain *" placeholder="contoso@boxyhq.com" />
-      <h2 class="hero-title">Login Component with default styles</h2>
-      <h2 class="hero-description">If classNames prop is not passed in, then default styling will be applied. Also supported is the passing of style attribute for each inner element (Note that inline style will override other styles).</h2>
-      <button @click="toggleCodeSnippet('loginDefaultStylesActive')" class="btn-toggle">Hide code</button>
-      <div v-if="loginDefaultStylesActive" class="component-code">
-        <pre><code class="language-js">        
+        <Login
+          :styles="{ input: { border: '1px solid darkcyan' } }"
+          @onSubmit="onSubmitButton"
+          inputLabel="Team domain *"
+          placeholder="contoso@boxyhq.com"
+        />
+        <h2 class="hero-title">Login Component with default styles</h2>
+        <h2 class="hero-description">
+          If classNames prop is not passed in, then default styling will be applied. Also supported is the passing of
+          style attribute for each inner element (Note that inline style will override other styles).
+        </h2>
+        <button @click="toggleCodeSnippet('loginDefaultStylesActive')" class="btn-toggle">Hide code</button>
+        <div v-if="loginDefaultStylesActive" class="component-code">
+          <pre><code class="language-js">        
 
 {{ defaultStylesComponentCode }}
       </code></pre>
+        </div>
       </div>
-    </div>
-    <div class="login-without-input">
-      <Login class="hero" ssoIdentifier="some-identifier" buttonText="SIGN IN WITH SSO" />
-      <h2 class="hero-title">Login Component without input display</h2>
-      <h2 class="hero-description">Here we pass the ssoIdentifier directly instead of taking a user input.</h2>
-      <button @click="toggleCodeSnippet('loginWithoutInputIsActive')" class="btn-toggle">Hide code</button>
-      <div v-if="loginWithoutInputIsActive">
-      <pre><code class="language-js">
+      <div class="login-without-input">
+        <Login class="hero" ssoIdentifier="some-identifier" buttonText="SIGN IN WITH SSO" />
+        <h2 class="hero-title">Login Component without input display</h2>
+        <h2 class="hero-description">Here we pass the ssoIdentifier directly instead of taking a user input.</h2>
+        <button @click="toggleCodeSnippet('loginWithoutInputIsActive')" class="btn-toggle">Hide code</button>
+        <div v-if="loginWithoutInputIsActive">
+          <pre><code class="language-js">
 {{ loginWithoutInput }}  
-      </code></pre></div>
-    </div>
-    <div class="login-with-failing-onsubmit">
-      <Login @onSubmit="onSubmitButton" :errorMessage="this.errorOnSubmit" class="child" inputLabel="Team domain *" placeholder="contoso@boxyhq.com" />
-      <h2 class="hero-title">Login Component with failing onSubmit</h2>
-      <h2 class="hero-description">If error object is returned with the error message, the message would be displayed inline.</h2>
-      <button @click="toggleCodeSnippet('loginWithFailingOnSubmitActive')" class="btn-toggle">Hide code</button>
-      <div v-if="loginWithFailingOnSubmitActive">
-      <pre><code class="language-js">
+      </code></pre>
+        </div>
+      </div>
+      <div class="login-with-failing-onsubmit">
+        <Login
+          @onSubmit="onSubmitButton"
+          :errorMessage="this.errorOnSubmit"
+          class="child"
+          inputLabel="Team domain *"
+          placeholder="contoso@boxyhq.com"
+        />
+        <h2 class="hero-title">Login Component with failing onSubmit</h2>
+        <h2 class="hero-description">
+          If error object is returned with the error message, the message would be displayed inline.
+        </h2>
+        <button @click="toggleCodeSnippet('loginWithFailingOnSubmitActive')" class="btn-toggle">Hide code</button>
+        <div v-if="loginWithFailingOnSubmitActive">
+          <pre><code class="language-js">
 {{ loginWithFailingOnSubmit }}
       </code></pre>
+        </div>
       </div>
-    </div>
     </div>
   </main>
 </template>
@@ -66,12 +94,17 @@ export default {
       loginDefaultStylesActive: true,
       loginWithFailingOnSubmitActive: true,
       loginWithCustomStylingActive: true,
-      errorOnSubmit: { error: {message: "Invalid team domain"}},
-      loginWithFailingOnSubmit:"<template>\n  <Login\n    :@onSubmit='async (ssoIdentifier) => {\n        // Initiate SSO flow here\n     }',\n    :errorMessage='{ error: { message:'Invalid team domain' } }',\n    inputLabel='Team domain *',\n    placeholder='contoso@boxyhq.com',\n  /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n\nexport default {\n  name:'Demo4'\n}\n< /script>",
-      loginWithCustomStyling:"<template>\n  <Login\n    :@onSubmit='async (ssoIdentifier) => {\n       // initiate  the SSO flow here\n     }'\n    :styles='{ input: { borderColor: '#ebedf0' }, button: { padding: '.85rem' } }',\n    :classNames='{button: 'btn', input: 'inp'}'\n    inputLabel='Team domain *',\n    placeholder='contos@boxyhq.com',\n    buttonText='Login with SSO'\n    :innerProps='{input: { type: 'email' }}'\n   /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n@import url('./demo1.css');\n\nexport default {\n  name:'Demo1'\n}\n< /script>",
-      defaultStylesComponentCode:"<template>\n  <Login\n    :styles='{ input: { border: '1px solid darkcyan' } }',\n    inputLabel='Team domain *',\n    placeholder='contos@boxyhq.com',\n   /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n\nexport default {\n  name:'Demo2'\n}\n< /script>",
-      defaultStylesCode:"<Login\n      :styles='{ input: { border: '1px solid darkcyan' } }',\n      inputLabel='Team domain *',\n      placeholder='contos@boxyhq.com'\n    />",
-      loginWithoutInput:"<template>\n  <Login\n    :@onSubmit='async (ssoIdentifier) => {\n       // initiate  the SSO flow here\n     }',\n    ssoIdentifier='some-identifier',\n    buttonText='SIGN IN WITH SSO',\n  /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n\nexport default {\n  name:'Demo3'\n}\n< /script>",
+      errorOnSubmit: { error: { message: 'Invalid team domain' } },
+      loginWithFailingOnSubmit:
+        "<template>\n  <Login\n    :@onSubmit='async (ssoIdentifier) => {\n        // Initiate SSO flow here\n     }',\n    :errorMessage='{ error: { message:'Invalid team domain' } }',\n    inputLabel='Team domain *',\n    placeholder='contoso@boxyhq.com',\n  /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n\nexport default {\n  name:'Demo4'\n}\n< /script>",
+      loginWithCustomStyling:
+        "<template>\n  <Login\n    :@onSubmit='async (ssoIdentifier) => {\n       // initiate  the SSO flow here\n     }'\n    :styles='{ input: { borderColor: '#ebedf0' }, button: { padding: '.85rem' } }',\n    :classNames='{button: 'btn', input: 'inp'}'\n    inputLabel='Team domain *',\n    placeholder='contos@boxyhq.com',\n    buttonText='Login with SSO'\n    :innerProps='{input: { type: 'email' }}'\n   /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n@import url('./demo1.css');\n\nexport default {\n  name:'Demo1'\n}\n< /script>",
+      defaultStylesComponentCode:
+        "<template>\n  <Login\n    :styles='{ input: { border: '1px solid darkcyan' } }',\n    inputLabel='Team domain *',\n    placeholder='contos@boxyhq.com',\n   /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n\nexport default {\n  name:'Demo2'\n}\n< /script>",
+      defaultStylesCode:
+        "<Login\n      :styles='{ input: { border: '1px solid darkcyan' } }',\n      inputLabel='Team domain *',\n      placeholder='contos@boxyhq.com'\n    />",
+      loginWithoutInput:
+        "<template>\n  <Login\n    :@onSubmit='async (ssoIdentifier) => {\n       // initiate  the SSO flow here\n     }',\n    ssoIdentifier='some-identifier',\n    buttonText='SIGN IN WITH SSO',\n  /> \n</template>\n\n<script>\nimport { Login } from '@boxyhq/vue3-ui';\n\nexport default {\n  name:'Demo3'\n}\n< /script>",
     };
   },
   methods: {
@@ -88,8 +121,11 @@ export default {
   mounted() {
     Prism.highlightAll();
   },
+  updated() {
+    Prism.highlightAll();
+  },
   created() {
     this.$emit('onSubmit');
-  }
+  },
 };
 </script>
