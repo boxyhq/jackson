8cb391e53ba46645986b8d59a767ff8beb45b461
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-09 12:28:18 +0530

    Update mitosis config to specify a target path for each target and updating target names

--- mitosis.config.js
+++ mitosis.config.js
@@ -1,11 +1,19 @@
+const getTargetPath = ({ target }) => {
+  return `${target}`;
+};
+
 /** @type {import('@builder.io/mitosis').MitosisConfig} */
 module.exports = {
   // The files that we want mitosis to compile
   files: 'sdk/ui/react/src/*',
-  targets: ['vue3', 'solid', 'svelte', 'react'],
+  targets: ['react', 'vue3', 'vue2', 'solid', 'angular', 'svelte'],
+  getTargetPath,
   options: {
     react: {
       stateType: 'useState',
+    },
+    vue3: {
+      api: 'composition',
     },
   },
 };
