0f2607d25464a29d4eadb9ca52cff108cd496cae
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-16 20:40:58 +0530

    Import login component in docs

--- sdk/ui/angular/projects/docs/src/app/app.module.ts
+++ sdk/ui/angular/projects/docs/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularUiComponent } from '@boxyhq/angular-ui'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularUiComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
