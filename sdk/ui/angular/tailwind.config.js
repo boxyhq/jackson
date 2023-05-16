2879ee95ff4683f59ce66ff247e86c4d0f90f03e
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-16 18:46:46 +0530

    Setup tailwind

---
+++ sdk/ui/angular/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+    "./projects/boxyhq/angular-ui/src/**/*.{html,ts}",
+    "./projects/docs/src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
