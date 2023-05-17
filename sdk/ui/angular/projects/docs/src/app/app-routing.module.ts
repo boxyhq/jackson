b116f939137e295e0f2a00b74ad0955f80b99a59
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-17 20:58:52 +0530

    Create routes for home and components view

--- sdk/ui/angular/projects/docs/src/app/app-routing.module.ts
+++ sdk/ui/angular/projects/docs/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component'
+import { ComponentsComponent } from './components/components.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+  },
+  {
+    path:'components',
+    component: ComponentsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
