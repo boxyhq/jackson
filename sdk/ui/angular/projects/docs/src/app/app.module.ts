0bd9f1453970eea19e0cdd0a8a38f8610cc5d79c
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-05-17 21:01:25 +0530

    Include componet views in the app imports

--- sdk/ui/angular/projects/docs/src/app/app.module.ts
+++ sdk/ui/angular/projects/docs/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularUiComponent } from '@boxyhq/angular-ui'
+import { AngularUiComponent } from '@boxyhq/angular-ui';
+import { HomeComponent } from './home/home.component';
+import { ComponentsComponent } from './components/components.component';
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularUiComponent
+    AngularUiComponent,
+    HomeComponent,
+    ComponentsComponent,
+    NavbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
