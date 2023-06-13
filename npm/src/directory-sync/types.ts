bbf2a68ac429f743df4626c79a3b53c02d89f95d
Deepak Prabhakara <deepak@boxyhq.com>
2023-06-13 18:24:52 +0100

    label tweak

--- npm/src/directory-sync/types.ts
+++ npm/src/directory-sync/types.ts
@@ -32,7 +32,7 @@ export enum DirectorySyncProviders {
   'onelogin-scim-v2' = 'OneLogin SCIM v2.0',
   'okta-scim-v2' = 'Okta SCIM v2.0',
   'jumpcloud-scim-v2' = 'JumpCloud v2.0',
-  'generic-scim-v2' = 'SCIM Generic v2.0',
+  'generic-scim-v2' = 'Generic SCIM v2.0',
   'google' = 'Google',
 }
 
