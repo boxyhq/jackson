2e3b337052197efb79fbe8c98acba62533a4edaf
Deepak Prabhakara <deepak@boxyhq.com>
2023-06-29 23:02:54 +0100

    bumped up timestamp

--- npm/migration/sql/1688075845553-namespace.ts
+++ npm/migration/sql/1688075845553-namespace.ts
@@ -1,14 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class namespace1688036519383 implements MigrationInterface {
-  name = 'namespace1688036519383'
+export class namespace1688075845553 implements MigrationInterface {
+  name = 'namespace1688075845553'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     const response = await queryRunner.query("select jackson.key from jackson_store jackson")
-    const responseJson = JSON.parse(JSON.stringify(response));
       const searchTerm = ':';
-      for (const k in responseJson) {
-        const key = responseJson[k].key;
+      for (const k in response) {
+        const key = response[k].key;
         const tokens2 = key.split(searchTerm).slice(0, 2);
         const value = tokens2.join(searchTerm);
         queryRunner.query(`update jackson_store set namespace = '${value}' where jackson_store.key = '${key}'`)
@@ -17,9 +16,8 @@ export class namespace1688036519383 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
       const response = await queryRunner.query("select jackson.key from jackson_store jackson")
-      const responseJson = JSON.parse(JSON.stringify(response));
-        for (const k in responseJson) {
-          const key = responseJson[k].key;
+        for (const k in response) {
+          const key = response[k].key;
           queryRunner.query(`update jackson_store set namespace = NULL where jackson_store.key = '${key}'`)
         }
   }
