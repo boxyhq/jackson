b1b54803f3dfdd70bb4a77018dd265d70762e08c
Kiran K <mailtokirankk@gmail.com>
2023-07-17 17:44:20 +0530

    add open API spec

--- npm/src/controller/setup-link.ts
+++ npm/src/controller/setup-link.ts
@@ -23,6 +23,31 @@ const throwIfInvalidService = (service: string) => {
   }
 };
 
+/**
+ * @swagger
+ * definitions:
+ *   SetupLink:
+ *      type: object
+ *      properties:
+ *        setupID:
+ *          type: string
+ *          description: Setup link ID
+ *        tenant:
+ *          type: string
+ *          description: Tenant
+ *        product:
+ *          type: string
+ *          description: Product
+ *        service:
+ *          type: string
+ *          description: Service name (sso or dsync)
+ *        validTill:
+ *          type: string
+ *          description: Valid till timestamp
+ *        url:
+ *          type: string
+ *          description: Setup link URL
+ */
 export class SetupLinkController {
   setupLinkStore: Storable;
 
@@ -30,7 +55,61 @@ export class SetupLinkController {
     this.setupLinkStore = setupLinkStore;
   }
 
-  // Create a new setup link
+  /**
+   * @swagger
+   * definitions:
+   *    SetupLink:
+   *      type: object
+   *      example:
+   *        {
+   *        	"data": {
+   *        		"setupID": "0689f76f7b5aa22f00381a124cb4b153fc1a8c08",
+   *        		"tenant": "acme",
+   *        		"product": "my-app",
+   *        		"service": "sso",
+   *        		"validTill": 1689849146690,
+   *        		"url": "http://localhost:5225/setup/0b96a483ebfe0af0b561dda35a96647074d944631ff9e070"
+   *        	}
+   *        }
+   * parameters:
+   *   serviceParamPost:
+   *     name: service
+   *     description: Service name (sso or dsync)
+   *     in: formData
+   *     required: true
+   *     type: string
+   *   tenantParamPost:
+   *     name: tenant
+   *     description: Tenant
+   *     in: formData
+   *     required: true
+   *     type: string
+   *   productParamPost:
+   *     name: product
+   *     description: Product
+   *     in: formData
+   *     required: true
+   *     type: string
+   * /api/v1/setup-links:
+   *   post:
+   *    summary: Create a new Setup Link
+   *    operationId: create-setup-link
+   *    tags: [Setup Link]
+   *    produces:
+   *      - application/json
+   *    consumes:
+   *      - application/x-www-form-urlencoded
+   *      - application/json
+   *    parameters:
+   *      - $ref: '#/parameters/serviceParamPost'
+   *      - $ref: '#/parameters/tenantParamPost'
+   *      - $ref: '#/parameters/productParamPost'
+   *    responses:
+   *      200:
+   *        description: Success
+   *        schema:
+   *          $ref:  '#/definitions/SetupLink'
+   */
   async create(body: SetupLinkCreatePayload): Promise<SetupLink> {
     const { tenant, product, service, name, description, defaultRedirectUrl, regenerate, redirectUrl } = body;
 
@@ -123,7 +202,32 @@ export class SetupLinkController {
     return setupLink[0];
   }
 
-  // Remove a setup link
+  /**
+   * @swagger
+   * parameters:
+   *   setupLinkId:
+   *     name: id
+   *     description: Setup link ID
+   *     in: path
+   *     required: true
+   *     type: string
+   * /api/v1/setup-links/{id}:
+   *   delete:
+   *     summary: Delete a Setup Link by ID
+   *     parameters:
+   *       - $ref: '#/parameters/setupLinkId'
+   *     operationId: delete-setup-link
+   *     tags: [Setup Link]
+   *     responses:
+   *      200:
+   *        description: Success
+   *        schema:
+   *          type: object
+   *          example:
+   *           {
+   *             data: {}
+   *           }
+   */
   async remove(key: string): Promise<boolean> {
     if (!key) {
       throw new JacksonError('Missing setup link key', 400);
@@ -139,6 +243,56 @@ export class SetupLinkController {
     return setupLink.validTill < +new Date();
   }
 
+  /**
+   * @swagger
+   * parameters:
+   *   serviceParamGet:
+   *     name: service
+   *     description: Service name (sso or dsync)
+   *     in: query
+   *     required: true
+   *     type: string
+   *   tenantParamGet:
+   *     name: tenant
+   *     description: Tenant
+   *     in: query
+   *     required: true
+   *     type: string
+   *   productParamGet:
+   *     name: product
+   *     description: Product
+   *     in: query
+   *     required: true
+   *     type: string
+   * /api/v1/setup-links:
+   *   get:
+   *     summary: Get the Setup Link for a tenant and product
+   *     parameters:
+   *       - $ref: '#/parameters/serviceParamGet'
+   *       - $ref: '#/parameters/tenantParamGet'
+   *       - $ref: '#/parameters/productParamGet'
+   *     operationId: set-setup-link-by-tenant-product-service
+   *     tags: [Setup Link]
+   *     responses:
+   *      200:
+   *        description: Success
+   *        schema:
+   *          $ref:  '#/definitions/SetupLink'
+   * /api/v1/setup-links/product:
+   *   get:
+   *     summary: Get the Setup Links for a product
+   *     parameters:
+   *       - $ref: '#/parameters/productParamGet'
+   *     operationId: set-setup-link-by-product
+   *     tags: [Setup Link]
+   *     responses:
+   *      200:
+   *        description: Success
+   *        schema:
+   *          type: array
+   *          items:
+   *            $ref:  '#/definitions/SetupLink'
+   */
   async filterBy(params: FilterByParams): Promise<{ data: SetupLink[]; pageToken?: string }> {
     const { tenant, product, service, pageOffset, pageLimit, pageToken } = params;
 
@@ -184,7 +338,28 @@ export class SetupLinkController {
     return { data: setupLinks, pageToken: nextPageToken };
   }
 
-  // Get a setup link by id
+  /**
+   * @swagger
+   * parameters:
+   *   idParamGet:
+   *     name: id
+   *     description: Setup Link ID
+   *     in: path
+   *     required: true
+   *     type: string
+   * /api/v1/setup-links/{id}:
+   *   get:
+   *     summary: Get the Setup Link by ID
+   *     parameters:
+   *       - $ref: '#/parameters/idParamGet'
+   *     operationId: get-setup-link-by-id
+   *     tags: [Setup Link]
+   *     responses:
+   *      200:
+   *        description: Success
+   *        schema:
+   *          $ref:  '#/definitions/SetupLink'
+   */
   async get(id: string): Promise<SetupLink> {
     if (!id) {
       throw new JacksonError('Missing setup link id', 400);
