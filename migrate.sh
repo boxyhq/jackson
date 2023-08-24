68573b16edf6637b66d42ca8897c1fb8904d54cd
Aswin V <vaswin91@gmail.com>
2023-08-25 00:28:43 +0530

    Cleanup migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [ "$RUN_MIGRATION" == "true" ]
-then
     echo "Initiating Migration..."
 
     cd ./npm
@@ -9,9 +7,9 @@ then
     then
         migrate-mongo up
     else
-        ts-node --transpile-only ./node_modules/typeorm/cli.js migration:run -d typeorm.ts
+    ts-node --transpile-only ./node_modules/typeorm/cli.js migration:run -d ./typeorm.ts
     fi
     echo "Migration Finished..."
 
     cd ..
-fi
+
