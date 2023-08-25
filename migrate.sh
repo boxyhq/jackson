28a76f60cb44d2b9f2209d02495d863e684f2b1e
Aswin V <vaswin91@gmail.com>
2023-08-25 16:47:22 +0530

    Source packages from global path

--- migrate.sh
+++ migrate.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 echo "Initiating Migration..."
+export NODE_PATH=$(npm root -g)
 
 cd ./npm
 if [ "$DB_ENGINE" = "mongo" ]
 then
     migrate-mongo up
 else
-    ts-node --transpile-only ./node_modules/typeorm/cli.js migration:run -d ./typeorm.ts
+    ts-node --transpile-only --project tsconfig.json $NODE_PATH/typeorm/cli.js migration:run -d ./typeorm.ts
 fi
 echo "Migration Finished..."
 
