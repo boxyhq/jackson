419affbbe1f6f96e6365603c0b9e59e72c763fd4
Aswin V <vaswin91@gmail.com>
2023-09-20 16:35:22 +0800

    Use `DirectoriesWrapper` from SDK

--- pages/admin/directory-sync/index.tsx
+++ pages/admin/directory-sync/index.tsx
@@ -1,9 +1,31 @@
 import type { GetStaticPropsContext, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import DirectoryList from '@components/dsync/DirectoryList';
+// import DirectoryList from '@components/dsync/DirectoryList';
+import { DirectoriesWrapper } from "@boxyhq/react-ui/sso";
+// import { useRouter } from 'next/router';
 
 const DirectoryIndexPage: NextPage = () => {
-  return <DirectoryList />;
+  // return <DirectoryList />
+
+  return <DirectoriesWrapper componentProps={{
+    directoryList: {
+      cols: ["name", "tenant", "product", "type"],
+      urls: { directories: '/api/admin/directory-sync', providers: '/api/admin/directory-sync/providers' }
+    },
+    createDirectory: {
+      urls: {
+        post: '/api/admin/directory-sync',
+        providers: '/api/admin/directory-sync/providers'
+      },
+    },
+    editDirectory: {
+      urls: {
+        patch: '/api/admin/directory-sync',
+        delete: '/api/admin/directory-sync',
+        get: '/api/admin/directory-sync/providers'
+      },
+    },
+  }} />
 };
 
 export const getStaticProps = async (context: GetStaticPropsContext) => {
