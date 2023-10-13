fbec8c882beacb5936c60531d0bdf9407fd68413
Aswin V <vaswin91@gmail.com>
2023-10-13 13:17:40 +0800

    Fix sdk css load (#1768)
    * Import main stylesheet from sdk
    
    * Override CSS and turn off header display

--- components/setup-link-instructions/CreateSSOConnection.tsx
+++ components/setup-link-instructions/CreateSSOConnection.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+
 import { CreateSAMLConnection as CreateSAML, CreateOIDCConnection as CreateOIDC } from '@boxyhq/react-ui/sso';
 
 import { errorToast } from '@components/Toaster';
@@ -26,10 +27,26 @@ const CreateSSOConnection = ({ setupLinkToken, idpType }: CreateSSOConnectionPro
     save: `/api/setup/${setupLinkToken}/sso-connection`,
   };
 
+  const _CSS = { input: 'input input-bordered', button: { ctoa: 'btn btn-primary' } };
+
   return idpType === 'saml' ? (
-    <CreateSAML variant='basic' urls={urls} successCallback={onSuccess} errorCallback={onError} />
+    <CreateSAML
+      variant='basic'
+      urls={urls}
+      successCallback={onSuccess}
+      errorCallback={onError}
+      classNames={_CSS}
+      displayHeader={false}
+    />
   ) : (
-    <CreateOIDC variant='basic' urls={urls} successCallback={onSuccess} errorCallback={onError} />
+    <CreateOIDC
+      variant='basic'
+      urls={urls}
+      successCallback={onSuccess}
+      errorCallback={onError}
+      classNames={_CSS}
+      displayHeader={false}
+    />
   );
 };
 
