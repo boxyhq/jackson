4c2cfaa683f4b6e0b643c1c6d37f20f758c7b2fe
Deepak Prabhakara <deepak@boxyhq.com>
2023-10-27 17:17:44 +0100

    updated daisyui

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  experimental: { webpackBuildWorker: true },
+  // experimental: { webpackBuildWorker: true },
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   i18n,
