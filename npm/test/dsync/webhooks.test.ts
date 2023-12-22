2992f36e842dee2a5e8c1aa1ed7c84e68dc67278
Kiran K <kiran@boxyhq.com>
2023-12-22 19:46:11 +0530

    Pass the okta runscope tests for SCIM (#2073)
    * Pass the okta runscope tests for SCIM
    
    * Fix duplicate group and user creation
    
    * Remove unused const
    
    * Update the tests
    
    * Refactor error response in DirectoryGroups.ts

--- npm/test/dsync/webhooks.test.ts
+++ npm/test/dsync/webhooks.test.ts
@@ -63,6 +63,7 @@ tap.test('Webhook Events /', async (t) => {
   t.test('Webhook Events / ', async (t) => {
     t.afterEach(async () => {
       await directorySync.webhookLogs.deleteAll(directory.id);
+      await directorySync.users.deleteAll(directory.id);
     });
 
     t.test("Should be able to get the directory's webhook", async (t) => {
