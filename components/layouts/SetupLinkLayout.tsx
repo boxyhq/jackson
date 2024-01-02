752d354498cc7827899480c8bb771b27b4ddbaca
Kiran K <mailtokirankk@gmail.com>
2024-01-02 13:28:27 +0530

    Update setup link branding

--- components/layouts/SetupLinkLayout.tsx
+++ components/layouts/SetupLinkLayout.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/router';
 import InvalidSetupLinkAlert from '@components/setup-link/InvalidSetupLinkAlert';
 import Loading from '@components/Loading';
 import useSetupLink from '@lib/ui/hooks/useSetupLink';
-import usePortalBranding from '@lib/ui/hooks/usePortalBranding';
 import { useTranslation } from 'next-i18next';
 import { hexToOklch } from '@lib/color';
 import { PoweredBy } from '@components/PoweredBy';
@@ -18,9 +17,8 @@ export const SetupLinkLayout = ({ children }: { children: React.ReactNode }) => 
   const { token } = router.query as { token: string };
 
   const { setupLink, error, isLoading } = useSetupLink(token);
-  const { branding, isLoading: isLoadingBranding } = usePortalBranding(setupLink?.product);
 
-  if (isLoading || isLoadingBranding) {
+  if (isLoading) {
     return <Loading />;
   }
 
@@ -34,19 +32,19 @@ export const SetupLinkLayout = ({ children }: { children: React.ReactNode }) => 
   return (
     <>
       <Head>
-        <title>{`${title} - ${branding?.companyName}`}</title>
-        {branding?.faviconUrl && <link rel='icon' href={branding.faviconUrl} />}
+        <title>{`${title} - ${setupLink?.companyName}`}</title>
+        {setupLink?.faviconUrl && <link rel='icon' href={setupLink.faviconUrl} />}
       </Head>
 
-      {branding?.primaryColor && <style>{`:root { --p: ${hexToOklch(branding.primaryColor)}; }`}</style>}
+      {setupLink?.primaryColor && <style>{`:root { --p: ${hexToOklch(setupLink.primaryColor)}; }`}</style>}
 
       <div className='mx-auto max-w-3xl'>
         <div className='flex flex-1 flex-col'>
           <div className='top-0 flex h-16 flex-shrink-0 border-b'>
             <div className='flex flex-shrink-0 items-center gap-4'>
               <Link href={`/setup/${token}`}>
-                {branding?.logoUrl && (
-                  <Image src={branding.logoUrl} alt={branding.companyName} width={40} height={40} />
+                {setupLink?.logoUrl && (
+                  <Image src={setupLink.logoUrl} alt={setupLink.companyName || ''} width={40} height={40} />
                 )}
               </Link>
               <span className='text-xl font-bold tracking-wide text-gray-900'>{title}</span>
--- lib/ui/hooks/useSetupLink.ts
+++ lib/ui/hooks/useSetupLink.ts
@@ -1,12 +1,15 @@
 import useSWR from 'swr';
-import type { SetupLink } from '@boxyhq/saml-jackson';
+import type { AdminPortalBranding, SetupLink } from '@boxyhq/saml-jackson';
 import type { ApiError, ApiSuccess } from 'types';
 import { fetcher } from '@lib/ui/utils';
 
 const useSetupLink = (setupLinkToken: string) => {
   const url = setupLinkToken ? `/api/setup/${setupLinkToken}` : null;
 
-  const { data, error, isLoading } = useSWR<ApiSuccess<SetupLink>, ApiError>(url, fetcher);
+  const { data, error, isLoading } = useSWR<ApiSuccess<SetupLink & AdminPortalBranding>, ApiError>(
+    url,
+    fetcher
+  );
 
   return {
     setupLink: data?.data,
