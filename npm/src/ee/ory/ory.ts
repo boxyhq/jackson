3ef236a2f80c761df4a65b114e3853bcad3b555b
Deepak Prabhakara <deepak@boxyhq.com>
2024-01-23 19:28:07 +0000

    update connection using project revision

--- npm/src/ee/ory/ory.ts
+++ npm/src/ee/ory/ory.ts
@@ -3,12 +3,62 @@ import axios, { AxiosError } from 'axios';
 import { throwIfInvalidLicense } from '../common/checkLicense';
 
 const basePath = 'https://api.console.ory.sh';
+const providerId = 'sso_boxyhq';
 
 export class OryController {
   private opts: JacksonOption;
 
   constructor({ opts }: { opts: JacksonOption }) {
     this.opts = opts;
+  }
+
+  private async addOrUpdateConnection(config: OryConfig): Promise<void> {
+    const project = await axios.get(`${basePath}/projects/${config.projectId}`, {
+      headers: {
+        Authorization: `Bearer ${config.sdkToken}`,
+      },
+    });
+
+    let index = '-';
+    try {
+      for (const idx in project.data.services.identity.config.selfservice.methods.oidc.config.providers) {
+        const provider = project.data.services.identity.config.selfservice.methods.oidc.config.providers[idx];
+        if (provider.id === providerId && provider.organization_id === config.organizationId) {
+          index = idx;
+          break;
+        }
+      }
+    } catch (err) {}
+
+    const op = index === '-' ? 'add' : 'replace';
+
+    await axios.patch(
+      `${basePath}/normalized/projects/${config.projectId}/revision/${project.data.revision_id}`,
+      [
+        { op: 'replace', path: '/kratos_selfservice_methods_oidc_enabled', value: true },
+        {
+          op,
+          path: `/kratos_selfservice_methods_oidc_config_providers/${index}`,
+          value: {
+            provider_id: providerId,
+            provider: 'generic',
+            label: 'SSO',
+            client_id: 'dummy',
+            client_secret: 'dummy',
+            organization_id: config.organizationId,
+            scope: [],
+            mapper_url: 'base64://eHl6',
+            additional_id_token_audiences: [],
+            issuer_url: 'https://sso.eu.boxyhq.com/.well-known/openid-configuration',
+          },
+        },
+      ],
+      {
+        headers: {
+          Authorization: `Bearer ${config.sdkToken}`,
+        },
+      }
+    );
   }
 
   private async createOrganization(config: OryConfig, label: string): Promise<string> {
@@ -63,15 +113,9 @@ export class OryController {
     }
 
     const organizationId = await this.createOrganization(config, label);
-    axios.patch(
-      `${basePath}/normalized/projects/${config.projectId}/revision/95106938-4a85-4cac-8788-4870e978a93f`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${config.sdkToken}`,
-        },
-      }
-    );
+
+    await this.addOrUpdateConnection(config);
+
     return { projectId: config.projectId, domains: config.domains, organizationId };
   }
 
@@ -88,6 +132,9 @@ export class OryController {
     }
 
     const organizationId = await this.createOrganization(config, label);
+
+    await this.addOrUpdateConnection(config);
+
     return { projectId: config.projectId, domains: config.domains, organizationId };
   }
 
