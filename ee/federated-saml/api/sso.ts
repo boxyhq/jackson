9b8d265af7a12cde78ee014414d79d27af5c0a24
Kiran K <mailtokirankk@gmail.com>
2024-02-05 16:27:04 +0530

    wip

--- ee/federated-saml/api/sso.ts
+++ ee/federated-saml/api/sso.ts
@@ -9,47 +9,75 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     switch (method) {
       case 'GET':
-        return await handleGET(req, res);
+        return await handleSAMLRequest(req, res);
+      case 'POST':
+        return await handleSAMLRequest(req, res, true);
       default:
-        res.setHeader('Allow', 'GET');
+        res.setHeader('Allow', 'GET, POST');
         res.status(405).json({ error: { message: `Method ${method} Not Allowed` } });
     }
   } catch (err: any) {
     console.error('authorize error:', err);
+
     const { message, statusCode = 500 } = err;
-    // set error in cookie redirect to error page
     setErrorCookie(res, { message, statusCode }, { path: '/error' });
     res.redirect(302, '/error');
   }
 }
 
+type SAMLRequest = {
+  SAMLRequest: string;
+  RelayState: string;
+};
+
 // Handle the SAML Request from Service Provider
 // This endpoint act as Single Sign On (SSO) URL
-const handleGET = async (req: NextApiRequest, res: NextApiResponse) => {
+async function handleSAMLRequest(req: NextApiRequest, res: NextApiResponse, isPostBinding = false) {
+  let samlRequest = '';
+  let relayState = '';
+  let idpHint = '';
+  let isDeflated = true;
+  let binding: 'http-redirect' | 'http-post' = 'http-redirect';
+
+  if (isPostBinding) {
+    const { SAMLRequest, RelayState } = req.body as SAMLRequest;
+
+    samlRequest = SAMLRequest;
+    relayState = RelayState;
+
+    binding = 'http-post';
+    isDeflated = false;
+  } else {
+    const { SAMLRequest, RelayState } = req.query as SAMLRequest;
+
+    samlRequest = SAMLRequest;
+    relayState = RelayState;
+    binding = 'http-post';
+  }
+
+  if ('idp_hint' in req.query) {
+    idpHint = req.query.idp_hint as string;
+  }
+
   const { samlFederatedController } = await jackson();
 
-  const { SAMLRequest, RelayState, idp_hint } = req.query as {
-    SAMLRequest: string;
-    RelayState: string;
-    idp_hint: string;
-  };
-
   const response = await samlFederatedController.sso.getAuthorizeUrl({
-    request: SAMLRequest,
-    relayState: RelayState,
-    idp_hint,
+    request: samlRequest,
+    relayState,
+    idpHint,
+    binding,
   });
 
   if (!response) {
     throw new Error('Unable to create SAML Federated request. Error creating authorize url.');
   }
 
-  if ('redirect_url' in response) {
+  if ('redirect_url' in response && response.redirect_url) {
     res.redirect(302, response.redirect_url);
   }
 
-  if ('authorize_form' in response) {
+  if ('authorize_form' in response && response.authorize_form) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     res.send(response.authorize_form);
   }
-};
+}
