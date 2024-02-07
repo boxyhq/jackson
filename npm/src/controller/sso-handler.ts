fd190bfc1fa7b58a0c4041111107c39b27aabbbc
Deepak Prabhakara <deepak@boxyhq.com>
2024-02-07 12:31:15 +0000

    added AttributeMapping type

--- npm/src/controller/sso-handler.ts
+++ npm/src/controller/sso-handler.ts
@@ -5,7 +5,7 @@ import { deflateRaw } from 'zlib';
 import type { SAMLProfile } from '@boxyhq/saml20/dist/typings';
 import { generators } from 'openid-client';
 
-import type { JacksonOption, Storable, SAMLSSORecord, OIDCSSORecord } from '../typings';
+import type { JacksonOption, Storable, SAMLSSORecord, OIDCSSORecord, AttributeMapping } from '../typings';
 import { getDefaultCertificate } from '../saml/x509';
 import * as dbutils from '../db/utils';
 import { JacksonError } from './error';
@@ -162,7 +162,7 @@ export class SSOHandler {
   }: {
     connection: SAMLSSORecord;
     requestParams: Record<string, any>;
-    mappings: any[] | null;
+    mappings: AttributeMapping[] | null;
   }) {
     // We have a connection now, so we can create the SAML request
     const certificate = await getDefaultCertificate();
@@ -235,7 +235,7 @@ export class SSOHandler {
   }: {
     connection: OIDCSSORecord;
     requestParams: Record<string, any>;
-    mappings: any[] | null;
+    mappings: AttributeMapping[] | null;
   }) {
     if (!this.opts.oidcPath) {
       throw new JacksonError('OpenID response handler path (oidcPath) is not set', 400);
@@ -341,7 +341,7 @@ export class SSOHandler {
     requested: any;
     oidcCodeVerifier?: string;
     oidcNonce?: string;
-    mappings: any[] | null;
+    mappings: AttributeMapping[] | null;
   }) => {
     const sessionId = crypto.randomBytes(16).toString('hex');
 
