13c69b4b0ae37e1eb41e42005bc6e778229d3e60
Deepak Prabhakara <deepak@boxyhq.com>
2024-02-12 14:38:47 +0000

    format fix

--- components/table/TableHeader.tsx
+++ components/table/TableHeader.tsx
@@ -1,5 +1,4 @@
-const theadClass =
-  'bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400';
+const theadClass = 'bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400';
 const trHeadClass = 'hover:bg-gray-50';
 const thClass = 'px-6 py-3';
 
@@ -8,7 +7,7 @@ export const TableHeader = ({ cols }: { cols: string[] }) => {
     <thead className={theadClass}>
       <tr className={trHeadClass}>
         {cols.map((col, index) => (
-          <th key={index} scope="col" className={thClass}>
+          <th key={index} scope='col' className={thClass}>
             {col}
           </th>
         ))}
