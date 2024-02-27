e41856cf39fd248313d3899746c5598728127425
Kiran K <mailtokirankk@gmail.com>
2024-02-27 10:49:57 +0530

    wip ttl

--- npm/src/directory-sync/scim/WebhookEventsLogger.ts
+++ npm/src/directory-sync/scim/WebhookEventsLogger.ts
@@ -13,6 +13,8 @@ type GetAllParams = PaginationParams & {
   directoryId?: string;
 };
 
+export const eventTTL = 60; // 7 days
+
 export class WebhookEventsLogger extends Base {
   constructor({ db }: { db: DatabaseStore }) {
     super({ db });
@@ -30,7 +32,7 @@ export class WebhookEventsLogger extends Base {
       delivered: status === 200,
     };
 
-    await this.store('logs').put(id, log, {
+    await this.eventStore().put(id, log, {
       name: 'directoryId',
       value: directory.id,
     });
@@ -39,7 +41,7 @@ export class WebhookEventsLogger extends Base {
   }
 
   public async get(id: string): Promise<WebhookEventLog> {
-    return await this.store('logs').get(id);
+    return await this.eventStore().get(id);
   }
 
   // Get the event logs for a directory paginated
@@ -54,16 +56,16 @@ export class WebhookEventsLogger extends Base {
         value: directoryId,
       };
 
-      eventLogs = (await this.store('logs').getByIndex(index, pageOffset, pageLimit)).data;
+      eventLogs = (await this.eventStore().getByIndex(index, pageOffset, pageLimit)).data;
     } else {
-      eventLogs = (await this.store('logs').getAll(pageOffset, pageLimit)).data;
+      eventLogs = (await this.eventStore().getAll(pageOffset, pageLimit)).data;
     }
 
     return eventLogs;
   }
 
   public async delete(id: string) {
-    await this.store('logs').delete(id);
+    await this.eventStore().delete(id);
   }
 
   // Delete all event logs for a directory
@@ -75,13 +77,18 @@ export class WebhookEventsLogger extends Base {
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      const { data: events } = await this.store('logs').getByIndex(index, 0, this.bulkDeleteBatchSize);
+      const { data: events } = await this.eventStore().getByIndex(index, 0, this.bulkDeleteBatchSize);
 
       if (!events || events.length === 0) {
         break;
       }
 
-      await this.store('logs').deleteMany(events.map((event) => event.id));
+      await this.eventStore().deleteMany(events.map((event) => event.id));
     }
+  }
+
+  // Get the store for the logs
+  private eventStore() {
+    return this.store('logs', eventTTL);
   }
 }
