c79731314179380a5f4817f8fee892a7fcc5fa96
Kiran K <mailtokirankk@gmail.com>
2024-02-29 12:01:27 +0530

    Display the Google auth button

--- pages/setup/[token]/directory-sync/[directoryId]/index.tsx
+++ pages/setup/[token]/directory-sync/[directoryId]/index.tsx
@@ -1,15 +1,33 @@
-import type { NextPage } from 'next';
 import React from 'react';
+import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import DirectoryInfo from '@components/dsync/DirectoryInfo';
+import { DirectoryInfo, LinkBack } from '@boxyhq/internal-ui';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { dsyncGoogleAuthURL } from '@lib/env';
 
 const DirectoryDetailsPage: NextPage = () => {
   const router = useRouter();
-
   const { token, directoryId } = router.query as { token: string; directoryId: string };
 
-  return <DirectoryInfo directoryId={directoryId} setupLinkToken={token} />;
+  return (
+    <div className='flex flex-col gap-4'>
+      <div>
+        <LinkBack href={`/setup/${token}/directory-sync`} />
+      </div>
+      <div>
+        <DirectoryInfo
+          urls={{
+            getDirectory: `/api/setup/${token}/directory-sync/${directoryId}`,
+            tabBase: '',
+            googleAuth: dsyncGoogleAuthURL,
+          }}
+          hideTabs={true}
+          displayGoogleAuthButton={true}
+        />
+      </div>
+    </div>
+  );
 };
 
 export const getServerSideProps = async (context) => {
