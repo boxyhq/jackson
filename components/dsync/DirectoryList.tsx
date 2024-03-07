aded50113c203ce515e2b649725538eaec0e8c74
Aswin V <vaswin91@gmail.com>
2024-03-07 14:21:22 +0530

    Fix router push

--- components/dsync/DirectoryList.tsx
+++ components/dsync/DirectoryList.tsx
@@ -39,12 +39,18 @@ const DSyncDirectoryList = ({ setupLinkToken }: { setupLinkToken?: string }) => 
           !isSetupLinkView
             ? {
                 itemsPerPage: pageLimit,
-                handlePageChange: ({ offset }) => {
-                  const currentOffset = router.query.offset;
-                  if (currentOffset !== `${offset}`) {
-                    router.query.offset = `${offset}`;
-                    router.push(router);
-                  }
+                handlePageChange: (payload) => {
+                  router.push(
+                    {
+                      pathname: router.pathname,
+                      query: {
+                        ...router.query,
+                        ...payload,
+                      },
+                    },
+                    undefined,
+                    { shallow: true }
+                  );
                 },
               }
             : undefined
