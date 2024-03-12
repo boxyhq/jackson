3aece35b7b8ecab0d9e626cca3f705addbada0e7
Aswin V <vaswin91@gmail.com>
2024-03-12 23:31:59 +0530

    Update component usage in Jackson UI

--- lib/env.ts
+++ lib/env.ts
@@ -122,5 +122,3 @@ export { retraced as retracedOptions };
 export { terminus as terminusOptions };
 export { apiKeys };
 export { jacksonOptions };
-
-export const dsyncGoogleAuthURL = externalUrl + '/api/scim/oauth/authorize';
