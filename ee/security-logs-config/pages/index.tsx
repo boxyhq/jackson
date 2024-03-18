b66da67a344786a0ac8ba8f06e4ffdb14f4c7447
ukrocks007 <ukrocks.mehta@gmail.com>
2024-03-18 15:15:24 +0530

    Refactor security logs configuration page to use internal UI Table component

--- ee/security-logs-config/pages/index.tsx
+++ ee/security-logs-config/pages/index.tsx
@@ -7,15 +7,15 @@ import { fetcher } from '@lib/ui/utils';
 import Loading from '@components/Loading';
 import EmptyState from '@components/EmptyState';
 import { LinkPrimary } from '@components/LinkPrimary';
-import { pageLimit, Pagination, NoMoreResults } from '@components/Pagination';
+import { pageLimit, Pagination, Table } from '@boxyhq/internal-ui';
 import usePaginate from '@lib/ui/hooks/usePaginate';
-import { IconButton } from '@components/IconButton';
 import PencilIcon from '@heroicons/react/24/outline/PencilIcon';
 import PlusIcon from '@heroicons/react/24/outline/PlusIcon';
 import router from 'next/router';
 import LicenseRequired from '@components/LicenseRequired';
 import { errorToast } from '@components/Toaster';
 import { getDisplayTypeFromSinkType } from '@lib/sinkConfigMap';
+import { TableBodyType } from 'internal-ui/src/shared/Table';
 
 const ConfigList = ({ hasValidLicense }: { hasValidLicense: boolean }) => {
   const { t } = useTranslation('common');
@@ -56,6 +56,66 @@ const ConfigList = ({ hasValidLicense }: { hasValidLicense: boolean }) => {
   const noConfigs = configs.length === 0 && paginate.offset === 0;
   const noMoreResults = configs.length === 0 && paginate.offset > 0;
 
+  const columns = [
+    {
+      key: 'name',
+      label: t('bui-dsync-name'),
+      wrap: true,
+      dataIndex: 'name',
+    },
+    {
+      key: 'type',
+      label: t('bui-dsync-type'),
+      wrap: true,
+      dataIndex: 'type',
+    },
+    {
+      key: 'tenant',
+      label: t('bui-dsync-tenant'),
+      wrap: true,
+      dataIndex: 'tenant',
+    },
+    {
+      key: 'actions',
+      label: t('bui-dsync-actions'),
+      wrap: true,
+      dataIndex: null,
+    },
+  ];
+
+  const cols = columns.map(({ label }) => label);
+
+  const body: TableBodyType[] = configs.map((config) => {
+    return {
+      id: config.id,
+      cells: columns.map((column) => {
+        const dataIndex = column.dataIndex as string;
+
+        if (dataIndex === null) {
+          return {
+            actions: [
+              {
+                text: t('bui-dsync-view'),
+                onClick: () => router.push(`/admin/settings/security-logs/${config.id}/edit`),
+                icon: <PencilIcon className='w-5' />,
+              },
+            ],
+          };
+        } else if (dataIndex === 'type') {
+          return {
+            wrap: column.wrap,
+            text: getDisplayTypeFromSinkType(config.type),
+          };
+        } else {
+        return {
+          wrap: column.wrap,
+          text: config[dataIndex],
+        };
+      }
+      }),
+    };
+  });
+
   return (
     <>
       <div className='mb-5 flex items-center justify-between'>
@@ -72,53 +132,7 @@ const ConfigList = ({ hasValidLicense }: { hasValidLicense: boolean }) => {
         </>
       ) : (
         <>
-          <div className='rounder border'>
-            <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-              <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
-                <tr className='hover:bg-gray-50'>
-                  <th scope='col' className='px-6 py-3'>
-                    {t('name')}
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                    {t('type')}
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                    {t('tenant')}
-                  </th>
-                  <th scope='col' className='px-6 py-3'>
-                    {t('actions')}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {configs &&
-                  configs.map((config) => {
-                    return (
-                      <tr
-                        key={config.id}
-                        className='border-b bg-white last:border-b-0 hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800'>
-                        <td className='px-6'>{config.name}</td>
-                        <td className='px-6'>{getDisplayTypeFromSinkType(config.type)}</td>
-                        <td className='px-6 py-3'>{config.tenant}</td>
-                        <td className='px-6'>
-                          <span className='inline-flex items-baseline'>
-                            <IconButton
-                              tooltip={t('edit')}
-                              Icon={PencilIcon}
-                              className='hover:text-green-400'
-                              onClick={() => {
-                                router.push(`/admin/settings/security-logs/${config.id}/edit`);
-                              }}
-                            />
-                          </span>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                {noMoreResults && <NoMoreResults colSpan={4} />}
-              </tbody>
-            </table>
-          </div>
+          <Table noMoreResults={noMoreResults} cols={cols} body={body} />
           <Pagination
             itemsCount={configs.length}
             offset={paginate.offset}
