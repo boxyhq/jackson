ad23c8a232b4ba875dd9b3111b54944e487e341d
Aswin V <vaswin91@gmail.com>
2024-03-20 12:45:30 +0530

    Override SDK styles

---
+++ components/styles.ts
@@ -0,0 +1,19 @@
+import styles from '@styles/sdk-override.module.css';
+
+export const BOXYHQ_UI_CSS = {
+  button: {
+    ctoa: 'btn btn-primary',
+    destructive: 'btn btn-md btn-error',
+  },
+  input: `${styles['sdk-input']} input input-bordered`,
+  select: styles['sdk-select'],
+  textarea: styles['sdk-input'],
+  confirmationPrompt: {
+    button: {
+      ctoa: 'btn-md',
+      cancel: 'btn-md btn-outline',
+    },
+  },
+  secretInput: 'input input-bordered',
+  section: 'mb-8',
+};
