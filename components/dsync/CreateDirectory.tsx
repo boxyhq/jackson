a4ba66aa705924d403b4666823500388d87868e1
Aswin V <vaswin91@gmail.com>
2024-03-21 19:12:56 +0530

    Tweak header text

--- components/dsync/CreateDirectory.tsx
+++ components/dsync/CreateDirectory.tsx
@@ -25,7 +25,7 @@ const CreateDirectory = ({
   return (
     <div>
       <LinkBack href={backUrl} />
-      <h2 className='mb-5 mt-5 font-bold text-gray-700 md:text-xl'>{t('new_directory')}</h2>
+      <h2 className='mb-5 mt-5 font-bold text-gray-700 md:text-xl'>{t('create_dsync_connection')}</h2>
       <div className='min-w-[28rem] rounded border border-gray-200 bg-white p-6 dark:border-gray-700 dark:bg-gray-800'>
         <CreateDSync
           displayHeader={false}
