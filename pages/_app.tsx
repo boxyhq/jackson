f02e38cb5840db45874650b06a439a801389b26a
Aswin V <vaswin91@gmail.com>
2024-03-22 00:22:25 +0530

    Revert sdk style import order - app styles should be latest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,8 @@ import micromatch from 'micromatch';
 import nextI18NextConfig from '../next-i18next.config.js';
 
 import { AccountLayout, SetupLinkLayout } from '@components/layouts';
-import '../styles/globals.css';
 import '@boxyhq/react-ui/dist/style.css';
+import '../styles/globals.css';
 import { BUIProvider } from '@boxyhq/internal-ui';
 
 const unauthenticatedRoutes = [
