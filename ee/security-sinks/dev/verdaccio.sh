7746cd47aaf69d7eb73e3e3745e694f01bff78e9
ukrocks007 <ukrocks.mehta@gmail.com>
2024-03-28 23:13:26 +0530

    Add verdaccio.sh script for publishing package to local registry

---
+++ ee/security-sinks/dev/verdaccio.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh -ex
+
+# TODO: Make this generic so everyone can run it
+
+VERSION=0.0.0
+
+# Unpublish the current version
+npm unpublish --registry http://localhost:4873/ @boxyhq/security-sinks@$VERSION
+
+# Build the package
+rm -rf dist
+npm run build
+
+# Publish
+npm publish --registry http://localhost:4873/
