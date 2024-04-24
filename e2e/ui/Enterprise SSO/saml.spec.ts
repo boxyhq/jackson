b889f184273a907785a60d442ad21704ec43f687
Aswin V <vaswin91@gmail.com>
2024-04-24 15:47:50 +0530

    Fix sso naming

--- e2e/ui/Enterprise SSO/saml.spec.ts
+++ e2e/ui/Enterprise SSO/saml.spec.ts
@@ -7,7 +7,7 @@ type MyFixtures = {
 export const test = baseTest.extend<MyFixtures>({
   samlPage: async ({ page, baseURL }, use, testInfo) => {
     const samlPage = new SAMLPage(page);
-    const ssoName = `single-connection-saml-${testInfo.workerIndex}`;
+    const ssoName = `saml-${testInfo.workerIndex}`;
     await samlPage.goto();
     await samlPage.addSSOConnection(ssoName, baseURL!);
     await use(samlPage);
@@ -16,8 +16,8 @@ export const test = baseTest.extend<MyFixtures>({
 });
 
 test('OAuth2 wrapper + SAML provider', async ({ samlPage, page, baseURL }, testInfo) => {
-  // check if the added connection appears in the connection list
-  await expect(page.getByText(`single-connection-saml-${testInfo.workerIndex}`)).toBeVisible();
+  // check if the first added connection appears in the connection list
+  await expect(page.getByText(`saml-${testInfo.workerIndex}-1`)).toBeVisible();
   // Logout of magic link login
   await samlPage.logout();
   await samlPage.signInWithSSO();
@@ -28,19 +28,19 @@ test('OAuth2 wrapper + SAML provider', async ({ samlPage, page, baseURL }, testI
 });
 
 test('OAuth2 wrapper + 2 SAML providers', async ({ samlPage, page, baseURL }, testInfo) => {
-  // check if the added connection appears in the connection list
-  await expect(page.getByText(`single-connection-saml-${testInfo.workerIndex}`)).toBeVisible();
+  const ssoName = `saml-${testInfo.workerIndex}`;
+  // check if the first added connection appears in the connection list
+  await expect(page.getByText(`${ssoName}-1`)).toBeVisible();
   // Add second SAML connection
-  const ssoName = `single-connection-saml-${testInfo.workerIndex}-2`;
   await samlPage.addSSOConnection(ssoName, baseURL!);
-  // check if the added connection appears in the connection list
-  await expect(page.getByText(ssoName)).toBeVisible();
+  // check if the second added connection appears in the connection list
+  await expect(page.getByText(`${ssoName}-2`)).toBeVisible();
   // Logout of magic link login
   await samlPage.logout();
   // Login using MockSAML
   await samlPage.signInWithSSO();
   // Select IdP from selection screen
-  await samlPage.selectIdP(ssoName);
+  await samlPage.selectIdP(`${ssoName}-2`);
   await samlPage.signInWithMockSAML();
   // Wait for browser to redirect back to admin portal
   await page.waitForURL((url) => url.origin === baseURL);
