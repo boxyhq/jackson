3ac932085ea3fc87d40d6853bd34abaf0c1495d0
ukrocks007 <ukrocks.mehta@gmail.com>
2024-05-02 17:52:57 +0530

    chore: Update Authorization header in e2e tests

--- e2e/api/helpers/api.ts
+++ e2e/api/helpers/api.ts
@@ -1,6 +1,6 @@
 export const options = {
   extraHTTPHeaders: {
-    Authorization: `Api-Key secret`,
+    Authorization: `Api-Key ${process.env.JACKSON_API_KEYS}`,
     'Content-Type': 'application/json',
   },
 };
