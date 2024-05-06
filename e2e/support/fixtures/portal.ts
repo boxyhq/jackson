f6479c900e0f400de0920715417cf8eb15657616
Aswin V <vaswin91@gmail.com>
2024-05-06 16:33:26 +0530

    Add credentials login to portal fixture

--- e2e/support/fixtures/portal.ts
+++ e2e/support/fixtures/portal.ts
@@ -5,6 +5,14 @@ export class Portal {
   constructor(public readonly page: Page) {
     this.userAvatarLocator = this.page.getByTestId('user-avatar');
   }
+
+  async doCredentialsLogin() {
+    await this.page.goto('/admin/auth/login');
+    await this.page.getByPlaceholder('Email').fill('super@boxyhq.com');
+    await this.page.getByPlaceholder('Password').fill('999login');
+    await this.page.getByRole('button', { name: 'Sign In' }).click();
+  }
+
   async isLoggedIn() {
     // assert login state
     await expect(this.userAvatarLocator).toBeVisible();
