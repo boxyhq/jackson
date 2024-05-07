438ff307239adf42f1f623887d466b545b9fa33c
Aswin V <vaswin91@gmail.com>
2024-05-07 17:03:32 +0530

    Add Admin Portal SSO via SAML Fed

--- e2e/ui/Identity Federation/saml.spec.ts
+++ e2e/ui/Identity Federation/saml.spec.ts
@@ -5,12 +5,24 @@ test('Create SAML Federated app', async ({ page }) => {
   await page.getByRole('link', { name: 'Apps' }).click();
   await expect(page).toHaveURL(/.*admin\/identity-federation/);
   await page.getByRole('button', { name: 'New App' }).click();
-  await page.getByPlaceholder('Your app').fill('SF-1');
-  await page.getByPlaceholder('example.com').fill('boxyhq.com');
-  await page.getByPlaceholder('MyApp').fill('demo');
-  await page.getByPlaceholder('https://your-sp.com/saml/acs').fill('http://localhost:5225/api/oauth/saml');
-  await page.getByPlaceholder('https://your-sp.com/saml/entityId').fill('http://saml.boxyhq.com');
+  await page.getByPlaceholder('Your app').and(page.getByLabel('Name')).fill('SF-1');
+  await page.getByPlaceholder('example.com').and(page.getByLabel('Tenant')).fill('acme.com');
+  await page.getByLabel('Product').fill('_jackson_admin_portal');
+  await page.getByLabel('ACS URL').fill('http://localhost:5225/api/oauth/saml');
+  await page.getByLabel('Entity ID / Audience URI / Audience Restriction').fill('http://saml.boxyhq.com');
   await page.getByRole('button', { name: 'Create App' }).click();
   await page.getByRole('link', { name: 'Back' }).click();
   await expect(page.getByRole('cell', { name: 'SF-1' })).toBeVisible();
+
+  // Add SAML connection for Admin portal
+  await page.getByRole('link', { name: 'Single Sign-On' }).click();
+  await page.getByTestId('create-connection').click();
+  await page.getByPlaceholder('MyApp').click();
+  await page.getByPlaceholder('MyApp').fill('SF-SAML-1');
+  await page.getByPlaceholder('Paste the Metadata URL here').click();
+  await page
+    .getByPlaceholder('Paste the Metadata URL here')
+    .fill('http://localhost:5225/.well-known/idp-metadata');
+  await page.getByRole('button', { name: 'Save' }).click();
+  await expect(page.getByRole('cell', { name: 'SF-SAML-1' })).toBeVisible();
 });
