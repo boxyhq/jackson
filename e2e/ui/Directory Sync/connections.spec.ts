464a5acbeba977d0270fa823a2e43be11647af00
Aswin V <vaswin91@gmail.com>
2024-05-15 00:39:33 +0530

    Simulate a user creation request to SCIM endpoint and assert it in view

--- e2e/ui/Directory Sync/connections.spec.ts
+++ e2e/ui/Directory Sync/connections.spec.ts
@@ -1,3 +1,7 @@
+import { test as baseTest, expect } from '@playwright/test';
+import { DSyncPage } from 'e2e/support/fixtures';
+import { getDirectory } from 'e2e/api/helpers/directories';
+import { createUser } from 'e2e/api/helpers/users';
 import { options } from 'e2e/api/helpers/api';
 
 type MyFixtures = {
@@ -13,6 +17,27 @@ export const test = baseTest.extend<MyFixtures>({
 });
 
 test.use(options);
+
+test('Azure SCIM connection', async ({ dsyncPage, request, page }) => {
+  await dsyncPage.addDSyncConnection('azure-scim-v2');
   //   Send API requests to user/groups endpoint
-  console.log(scimUrl, scimToken);
+  const [directory] = await getDirectory(request, { tenant: dsyncPage.tenant, product: dsyncPage.product });
+  await createUser(request, directory, {
+    schemas: [
+      'urn:ietf:params:scim:schemas:core:2.0:User',
+      'urn:ietf:params:scim:schemas:extension:enterprise:2.0:User',
+    ],
+    externalId: 'jackson',
+    userName: 'jackson@aswinboxyhq.onmicrosoft.com',
+    active: true,
+    displayName: 'Jackson',
+    emails: [{ primary: true, type: 'work', value: 'jackson@example.com' }],
+    meta: { resourceType: 'User' },
+    name: { formatted: 'givenName familyName', familyName: 'familyName', givenName: 'givenName' },
+    title: 'Manager',
+  });
+  await dsyncPage.switchToUsersView();
+  expect(await page.getByRole('cell', { name: 'givenName' })).toBeVisible();
+  expect(await page.getByRole('cell', { name: 'familyName' })).toBeVisible();
+  expect(await page.getByRole('cell', { name: 'jackson@example.com' })).toBeVisible();
 });
