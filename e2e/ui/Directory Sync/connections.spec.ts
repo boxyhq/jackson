af3789a9c659ad90b900f4a9fe460abb47cfbd39
Aswin V <vaswin91@gmail.com>
2024-05-16 16:41:34 +0530

    Assert webhook events

--- e2e/ui/Directory Sync/connections.spec.ts
+++ e2e/ui/Directory Sync/connections.spec.ts
@@ -27,18 +27,35 @@ test('Azure SCIM connection', async ({ dsyncPage, request, page }) => {
   const user1 = await createUser(request, directory, azureUser1);
   const group = await createGroup(request, directory, azureGroup);
   await addGroupMember(request, directory, group, user1.id);
+  // Assert created user
   await dsyncPage.switchToUsersView();
   expect(await page.getByRole('cell', { name: azureUser1.name.givenName, exact: true })).toBeVisible();
   expect(await page.getByRole('cell', { name: azureUser1.name.familyName, exact: true })).toBeVisible();
   expect(await page.getByRole('cell', { name: azureUser1.emails[0].value, exact: true })).toBeVisible();
+  // Assert created group
   await dsyncPage.switchToGroupsView();
   expect(await page.getByRole('cell', { name: 'BoxyHQ' })).toBeVisible();
+  // Enable webhook logs
   await dsyncPage.enableWebHookEventLogging();
   const azureUser2 = azureUser(2);
   const user2 = await createUser(request, directory, azureUser2);
   await addGroupMember(request, directory, group, user2.id);
+  // Assert created user
   await dsyncPage.switchToUsersView();
   expect(await page.getByRole('cell', { name: azureUser2.name.givenName, exact: true })).toBeVisible();
   expect(await page.getByRole('cell', { name: azureUser2.name.familyName, exact: true })).toBeVisible();
   expect(await page.getByRole('cell', { name: azureUser2.emails[0].value, exact: true })).toBeVisible();
+  await page.getByText('Webhook Events').click();
+  const webhookRowRegex = new RegExp(`${directory.webhook.endpoint}.*View`);
+  await page.getByRole('row', { name: webhookRowRegex }).getByRole('button').first().click();
+  await page.waitForURL('**/admin/directory-sync/**/events/**');
+  await page.getByText('Loading...').waitFor();
+  await page.locator('pre').waitFor();
+  expect(await page.getByText('"group.user_added"')).toBeVisible();
+  await page.getByRole('listitem').and(page.getByText('Webhook Events')).click();
+  await page.getByRole('row', { name: webhookRowRegex }).getByRole('button').nth(1).click();
+  await page.waitForURL('**/admin/directory-sync/**/events/**');
+  await page.getByText('Loading...').waitFor();
+  await page.locator('pre').waitFor();
+  expect(await page.getByText('"user.created"')).toBeVisible();
 });
