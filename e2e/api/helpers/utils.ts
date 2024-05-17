392f69eb53b4d31f8da3fc7f2df0ae45fa227b7b
Aswin V <vaswin91@gmail.com>
2024-05-17 17:01:40 +0530

    Tweak scimEndpoint creation

--- e2e/api/helpers/utils.ts
+++ e2e/api/helpers/utils.ts
@@ -4,7 +4,7 @@ export function scimOpUrl(directory: Directory, opPath: string) {
   let endpoint = `${directory.scim.endpoint}/${opPath}`;
   if (directory.type === 'azure-scim-v2') {
     const [_main, aadOpts] = directory.scim.endpoint!.split('?');
-    endpoint = `${_main}${opPath}/?${aadOpts}`;
+    endpoint = `${_main}${opPath}?${aadOpts}`;
   }
   return endpoint;
 }
