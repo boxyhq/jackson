177ab879d4e0ad2e12ac519a846b1f36e5a6645f
Aswin V <vaswin91@gmail.com>
2024-05-20 16:37:49 +0530

    Add missing await for visibility checks

--- e2e/ui/Directory Sync/connections.spec.ts
+++ e2e/ui/Directory Sync/connections.spec.ts
@@ -37,12 +37,12 @@ test('Azure SCIM connection', async ({ dsyncPage, request, page }) => {
   // Assert created user
   await dsyncPage.switchToDSyncInfoView();
   await dsyncPage.switchToUsersView({ waitForData: true });
-  expect(await page.getByRole('cell', { name: azureUser1.name.givenName, exact: true })).toBeVisible();
-  expect(await page.getByRole('cell', { name: azureUser1.name.familyName, exact: true })).toBeVisible();
-  expect(await page.getByRole('cell', { name: azureUser1.emails[0].value, exact: true })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureUser1.name.givenName, exact: true })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureUser1.name.familyName, exact: true })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureUser1.emails[0].value, exact: true })).toBeVisible();
   // Assert created group
   await dsyncPage.switchToGroupsView({ waitForData: true, waitForResponse: true });
-  expect(await page.getByRole('cell', { name: 'BoxyHQ' })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: 'BoxyHQ' })).toBeVisible();
   // Enable webhook logs
   await dsyncPage.enableWebHookEventLogging();
   const azureUser2 = azureUser(2);
@@ -51,34 +51,37 @@ test('Azure SCIM connection', async ({ dsyncPage, request, page }) => {
   // Assert created user
   await dsyncPage.switchToDSyncInfoView();
   await dsyncPage.switchToUsersView({ waitForData: true });
-  expect(await page.getByRole('cell', { name: azureUser2.name.givenName, exact: true })).toBeVisible();
-  expect(await page.getByRole('cell', { name: azureUser2.name.familyName, exact: true })).toBeVisible();
-  expect(await page.getByRole('cell', { name: azureUser2.emails[0].value, exact: true })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureUser2.name.givenName, exact: true })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureUser2.name.familyName, exact: true })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureUser2.emails[0].value, exact: true })).toBeVisible();
   // Assert webhook logs
   await dsyncPage.switchToEventsView({ waitForData: true });
   await dsyncPage.inspectEventRow(0, directory.webhook.endpoint);
-  expect(await page.getByText('"group.user_added"')).toBeVisible();
+  await expect(await page.getByText('"group.user_added"')).toBeVisible();
   await dsyncPage.switchToEventsView({ waitForData: true });
   await dsyncPage.inspectEventRow(1, directory.webhook.endpoint);
-  expect(await page.getByText('"user.created"')).toBeVisible();
+  await expect(await page.getByText('"user.created"')).toBeVisible();
   // Delete webhook logs
   await dsyncPage.switchToEventsView();
   await page.getByRole('button', { name: 'Remove Events' }).click();
   await page.getByTestId('confirm-delete').click();
   await page.getByRole('table').waitFor({ state: 'detached' });
-  expect(
+  await expect(
     await page.getByRole('heading', { name: 'No webhook events found for this directory.' })
   ).toBeVisible();
   // User deletion
   await deleteUser(request, directory, user1.id);
   await deleteUser(request, directory, user2.id);
   await dsyncPage.switchToUsersView();
-  expect(await page.getByRole('heading', { name: 'No users found for this directory.' })).toBeVisible();
+  await expect(await page.getByRole('heading', { name: 'No users found for this directory.' })).toBeVisible();
   await updateGroupName(request, directory, group.id, azureGroupUpdatedName);
   await dsyncPage.switchToGroupsView({ waitForData: true, waitForResponse: true });
-  expect(await page.getByRole('cell', { name: azureGroupUpdatedName })).toBeVisible();
+  await expect(await page.getByRole('cell', { name: azureGroupUpdatedName })).toBeVisible();
   // Group deletion
   await deleteGroup(request, directory, group.id);
-  await dsyncPage.switchToGroupsView({ waitForResponse: true });
-  expect(await page.getByRole('heading', { name: 'No groups found for this directory.' })).toBeVisible();
+  // await dsyncPage.switchToGroupsView({ waitForResponse: true });
+  await page.reload();
+  await expect(
+    await page.getByRole('heading', { name: 'No groups found for this directory.' })
+  ).toBeVisible();
 });
