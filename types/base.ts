54c8b38600c482ac73915ac33958e1a0c7b9588f
Aswin V <vaswin91@gmail.com>
2024-05-28 12:31:42 +0530

    Revert type change and remove unused type

--- types/base.ts
+++ types/base.ts
@@ -1,10 +1,8 @@
 export type ApiSuccess<T> = { data: T; pageToken?: string };
 
 export interface ApiError extends Error {
-  // info?: string;
+  info?: string;
   status: number;
 }
-
-export type ApiResponse<T = any> = ApiSuccess<T> | { error: ApiError };
 
 export type PaginateApiParams = { pageOffset: number; pageLimit: number } & { pageToken?: string };
