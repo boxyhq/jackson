a6ae7f3fe09c75662aee99ef374d3e1f05f484db
Aswin V <vaswin91@gmail.com>
2024-06-11 18:42:33 +0530

    Rename identifier

--- npm/src/controller/oauth.ts
+++ npm/src/controller/oauth.ts
@@ -412,7 +412,7 @@ export class OAuthController implements IOAuthController {
         const standardScopes = this.opts.openid?.requestProfileScope
           ? ['openid', 'email', 'profile']
           : ['openid', 'email'];
-        const authzParams = this.opts.openid?.forwardOIDCParams ? oidcParams : {};
+        const paramsToForward = this.opts.openid?.forwardOIDCParams ? oidcParams : {};
         ssoUrl = oidcClient.authorizationUrl({
           scope: [...requestedScopes, ...standardScopes]
             .filter((value, index, self) => self.indexOf(value) === index) // filter out duplicates
@@ -422,7 +422,7 @@ export class OAuthController implements IOAuthController {
           state: relayState,
           nonce: oidcNonce,
           login_hint,
-          ...authzParams,
+          ...paramsToForward,
         });
       } catch (err: unknown) {
         const error_description = (err as errors.OPError)?.error || getErrorMessage(err);
