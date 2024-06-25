6cddfd94133dd64c05716925e4e4a3495afaf866
Aswin V <vaswin91@gmail.com>
2024-06-25 12:28:00 +0530

    Add sso add method to portal fixture

--- e2e/support/fixtures/portal.ts
+++ e2e/support/fixtures/portal.ts
@@ -6,6 +6,39 @@ export class Portal {
     this.userAvatarLocator = this.page.getByTestId('user-avatar');
   }
 
+  async addSSOConnection({
+    name,
+    type = 'saml',
+    metadataUrl,
+    oidcDiscoveryUrl,
+    oidcClientId,
+    oidcClientSecret,
+  }: {
+    name: string;
+    type?: 'saml' | 'oidc';
+    metadataUrl?: string;
+    oidcDiscoveryUrl?: string;
+    oidcClientId?: string;
+    oidcClientSecret?: string;
+  }) {
+    await this.page.getByRole('link', { name: 'Single Sign-On' }).click();
+    await this.page.getByTestId('create-connection').click();
+    if (type === 'oidc') {
+      await this.page.getByLabel('OIDC').check();
+    }
+    await this.page.getByLabel('Connection name (Optional)').fill(name);
+    if (type === 'saml') {
+      await this.page.getByPlaceholder('Paste the Metadata URL here').fill(metadataUrl!);
+    }
+    if (type === 'oidc') {
+      await this.page.getByLabel('Client ID').fill(oidcClientId!);
+      await this.page.getByLabel('Client Secret').fill(oidcClientSecret!);
+      await this.page.getByLabel('Well-known URL of OpenID Provider').fill(oidcDiscoveryUrl!);
+    }
+    await this.page.getByRole('button', { name: 'Save' }).click();
+    await expect(this.page.getByRole('cell', { name })).toBeVisible();
+  }
+
   async doCredentialsLogin() {
     await this.page.goto('/admin/auth/login');
     await this.page.getByPlaceholder('Email').fill('super@boxyhq.com');
