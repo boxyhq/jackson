ad918cf5e151b49c8cceb761566cee6bfa521295
Aswin V <vaswin91@gmail.com>
2024-07-18 13:43:58 +0530

    `createConversation`, `createChat` and `getConversationById`

--- npm/src/ee/chat/index.ts
+++ npm/src/ee/chat/index.ts
@@ -1,17 +1,27 @@
 // import { JacksonError } from '../../controller/error';
+import crypto from 'crypto';
 import { throwIfInvalidLicense } from '../common/checkLicense';
 import type { Storable, JacksonOption, Records } from '../../typings';
-import { LLMConversation } from './types';
+import { LLMChat, LLMConversation } from './types';
 import { IndexNames } from '../../controller/utils';
 import * as dbutils from '../../db/utils';
 
 export class ChatController {
-  // private chatStore: Storable;
+  private chatStore: Storable;
   // private configStore: Storable;
   private conversationStore: Storable;
   private opts: JacksonOption;
 
-  constructor({ conversationStore, opts }: { conversationStore: Storable; opts: JacksonOption }) {
+  constructor({
+    chatStore,
+    conversationStore,
+    opts,
+  }: {
+    chatStore: Storable;
+    conversationStore: Storable;
+    opts: JacksonOption;
+  }) {
+    this.chatStore = chatStore;
     this.conversationStore = conversationStore;
     this.opts = opts;
   }
@@ -36,5 +46,35 @@ export class ChatController {
     })) as Records<LLMConversation>;
 
     return conversations;
+  }
+
+  public async getConversationById(conversationId: string): Promise<LLMConversation> {
+    await throwIfInvalidLicense(this.opts.boxyhqLicenseKey);
+
+    const conversation = (await this.conversationStore.get(conversationId)) as LLMConversation;
+
+    return conversation;
+  }
+
+  public async createConversation(
+    conversation: Exclude<LLMConversation, 'id' | 'LLMChat'>
+  ): Promise<LLMConversation> {
+    await throwIfInvalidLicense(this.opts.boxyhqLicenseKey);
+
+    const conversationID = crypto.randomBytes(20).toString('hex');
+
+    const newConversation = await this.conversationStore.put(conversationID, conversation);
+
+    return newConversation;
+  }
+
+  public async createChat(chat: LLMChat): Promise<LLMChat> {
+    await throwIfInvalidLicense(this.opts.boxyhqLicenseKey);
+
+    const chatID = crypto.randomBytes(20).toString('hex');
+
+    const newChat = await this.chatStore.put(chatID, chat);
+
+    return newChat;
   }
 }
