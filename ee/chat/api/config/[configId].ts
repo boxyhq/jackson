e2c72786305e4351b3b8b61cab59caa82fc57ec9
Aswin V <vaswin91@gmail.com>
2024-07-23 13:51:07 +0530

    API route for updating/deleting LLM configs

---
+++ ee/chat/api/config/[configId].ts
@@ -0,0 +1,63 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { deleteLLMConfigSchema, updateLLMConfigSchema, validateWithSchema } from '@lib/zod';
+import jackson from '@lib/jackson';
+import { LLMProvider } from '@lib/llm';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    switch (req.method) {
+      case 'DELETE':
+        await handleDELETE(req, res);
+        break;
+      case 'PUT':
+        await handlePUT(req, res);
+        break;
+      default:
+        res.setHeader('Allow', 'DELETE, PUT');
+        res.status(405).json({
+          error: { message: `Method ${req.method} Not Allowed` },
+        });
+    }
+  } catch (error: any) {
+    const { message, statusCode = 500 } = error;
+    res.status(statusCode).json({ error: { message } });
+  }
+}
+
+// Delete llm config
+const handleDELETE = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { chatController } = await jackson();
+
+  const { configId } = validateWithSchema(deleteLLMConfigSchema, req.query);
+
+  await chatController.deleteLLMConfig({
+    configId,
+    tenant: req.query.tenant as string,
+  });
+
+  res.status(204).end();
+};
+
+// Update llm config
+const handlePUT = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { chatController } = await jackson();
+
+  const { configId, tenant, apiKey, models, baseURL, piiPolicy, provider } = validateWithSchema(
+    updateLLMConfigSchema,
+    {
+      ...req.body,
+      ...req.query,
+    }
+  );
+
+  await chatController.updateLLMConfig(configId, {
+    tenant,
+    apiKey,
+    baseURL,
+    piiPolicy,
+    provider: provider as LLMProvider,
+    models,
+  });
+
+  res.status(204).end();
+};
