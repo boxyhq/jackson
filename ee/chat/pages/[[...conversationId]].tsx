918effbb79a19e6663d1f97e1fc08952ca6dcc0c
Aswin V <vaswin91@gmail.com>
2024-07-31 23:08:23 +0530

    Move under ee pages, add license check

---
+++ ee/chat/pages/[[...conversationId]].tsx
@@ -0,0 +1,25 @@
+import { ChatContextProvider, ChatUI } from '@boxyhq/internal-ui';
+import LicenseRequired from '@components/LicenseRequired';
+
+const ChatPage = ({ llmTenant, hasValidLicense }: { llmTenant: string; hasValidLicense: boolean }) => {
+  if (!hasValidLicense) {
+    return <LicenseRequired />;
+  }
+
+  return (
+    <ChatContextProvider
+      value={{
+        urls: {
+          chat: `/api/admin/chat/${llmTenant}`,
+          llmConfig: `/api/admin/chat/${llmTenant}/config`,
+          llmProviders: `/api/admin/chat/${llmTenant}/providers`,
+          fileUpload: `/api/admin/chat/${llmTenant}/file-upload`,
+          conversation: `/api/admin/chat/${llmTenant}/conversation`,
+        },
+      }}>
+      <ChatUI />
+    </ChatContextProvider>
+  );
+};
+
+export default ChatPage;
--- pages/admin/chat/[[...conversationId]].tsx
+++ pages/admin/chat/[[...conversationId]].tsx
@@ -1,32 +1,18 @@
-import { ChatContextProvider, ChatUI } from '@boxyhq/internal-ui';
 import { llmOptions } from '@lib/env';
-import type { GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from 'next';
+import type { GetServerSidePropsContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import jackson from '@lib/jackson';
 
-const ChatPage: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ llmTenant }) => {
-  return (
-    <ChatContextProvider
-      value={{
-        urls: {
-          chat: `/api/admin/chat/${llmTenant}`,
-          llmConfig: `/api/admin/chat/${llmTenant}/config`,
-          llmProviders: `/api/admin/chat/${llmTenant}/providers`,
-          fileUpload: `/api/admin/chat/${llmTenant}/file-upload`,
-          conversation: `/api/admin/chat/${llmTenant}/conversation`,
-        },
-      }}>
-      <ChatUI />
-    </ChatContextProvider>
-  );
-};
+export { default } from '@ee/chat/pages/[[...conversationId]]';
 
 export async function getServerSideProps({ locale }: GetServerSidePropsContext) {
+  const { checkLicense } = await jackson();
+
   return {
     props: {
       ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
       llmTenant: llmOptions.adminPortalTenant,
+      hasValidLicense: await checkLicense(),
     },
   };
 }
-
-export default ChatPage;
