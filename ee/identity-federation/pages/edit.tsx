c8157a12e3ac184345d75587c044aeac46156087
Aswin V <vaswin91@gmail.com>
2024-08-25 00:34:31 +0700

    UI display of initiate_login_uri

--- ee/identity-federation/pages/edit.tsx
+++ ee/identity-federation/pages/edit.tsx
@@ -5,6 +5,7 @@ import LicenseRequired from '@components/LicenseRequired';
 import { EditIdentityFederationApp, LinkBack } from '@boxyhq/internal-ui';
 
 import 'react-tagsinput/react-tagsinput.css';
+import { jacksonOptions } from '@lib/env';
 
 const UpdateApp = ({ hasValidLicense }: { hasValidLicense: boolean }) => {
   const router = useRouter();
@@ -24,6 +25,7 @@ const UpdateApp = ({ hasValidLicense }: { hasValidLicense: boolean }) => {
           getApp: `/api/admin/identity-federation/${id}`,
           updateApp: `/api/admin/identity-federation/${id}`,
           deleteApp: `/api/admin/identity-federation/${id}`,
+          jacksonUrl: jacksonOptions.externalUrl,
         }}
         onUpdate={() => {
           successToast(t('identity_federation_update_success'));
