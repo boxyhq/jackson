25a04044ee5eff8aa558ec1eee44a804f15ff12e
Aswin V <vaswin91@gmail.com>
2024-09-02 17:59:17 +0700

    Trace the error thrown

--- npm/src/ee/identity-federation/idp-login.ts
+++ npm/src/ee/identity-federation/idp-login.ts
@@ -1,6 +1,13 @@
 import { JacksonError } from '../../controller/error';
 import { SSOHandler } from '../../controller/sso-handler';
+import { getErrorMessage, isConnectionActive } from '../../controller/utils';
+import {
+  IdentityFederationApp,
+  JacksonOption,
+  OIDCIdPInitiatedReq,
+  OIDCSSORecord,
   SSOTracesInstance,
+} from '../../typings';
 import { throwIfInvalidLicense } from '../common/checkLicense';
 import { App } from './app';
 
@@ -10,7 +17,7 @@ export class IdPLogin {
   private app: App;
   private opts: JacksonOption;
 
-  constructor({ connectionStore, sessionStore, app, opts }) {
+  constructor({ app, ssoHandler, ssoTraces, opts }) {
     this.app = app;
     this.ssoHandler = ssoHandler;
     this.ssoTraces = ssoTraces;
@@ -24,60 +31,82 @@ export class IdPLogin {
     await throwIfInvalidLicense(this.opts.boxyhqLicenseKey);
 
     let connection: OIDCSSORecord | undefined;
+    let fedApp: IdentityFederationApp | undefined;
 
-    const { iss, target_link_uri, idp_hint } = body;
+    const { iss, target_link_uri } = body;
 
-    // get federated connection
-    const fedApp = await this.app.get({
-      id: fedAppId,
-    });
+    try {
+      // get federated connection
+      fedApp = await this.app.get({
+        id: fedAppId,
+      });
 
-    if (fedApp.type !== 'saml') {
-      throw new JacksonError(
-        'Third party login from an OIDC provider is only supported with SAML Federation',
-        400
-      );
+      if (fedApp.type !== 'saml') {
+        throw new JacksonError(
+          'Third party login from an OIDC provider is only supported with SAML Federation',
+          400
+        );
+      }
+
+      const requestedTenant = fedApp.tenant;
+      const requestedProduct = fedApp.product;
+
+      const response = await this.ssoHandler.resolveConnection({
+        tenant: requestedTenant,
+        product: requestedProduct,
+        authFlow: 'idp-initiated',
+        originalParams: { ...body },
+        tenants: fedApp.tenants,
+        idFedAppId: fedApp.id,
+        fedType: fedApp.type, // will be saml
+        thirdPartyLogin: { idpInitiatorType: 'oidc', iss, target_link_uri },
+      });
+
+      if ('connection' in response) {
+        connection = response.connection as OIDCSSORecord;
+      }
+
+      if (!connection) {
+        throw new JacksonError('IdP connection not found.', 404);
+      }
+
+      if (!isConnectionActive(connection)) {
+        throw new JacksonError('OIDC connection is deactivated. Please contact your administrator.', 403);
+      }
+
+      const requestParams = {
+        acsUrl: fedApp.acsUrl,
+        entityId: fedApp.entityId,
+        tenant: fedApp.tenant,
+        product: fedApp.product,
+        relayState: target_link_uri,
+      };
+
+      return await this.ssoHandler.createOIDCRequest({
+        connection,
+        requestParams,
+        mappings: fedApp.mappings,
+      });
+    } catch (err: unknown) {
+      const error_description = getErrorMessage(err);
+
+      this.ssoTraces.saveTrace({
+        error: error_description,
+        context: {
+          tenant: fedApp?.tenant || '',
+          product: fedApp?.product || '',
+          clientID: connection?.clientID || '',
+          isSAMLFederated: true,
+          relayState: target_link_uri,
+          providerName: connection?.oidcProvider.friendlyProviderName || '',
+          acsUrl: fedApp?.acsUrl,
+          entityId: fedApp?.entityId,
+          isIdPFlow: true,
+          oidcIdPRequest: body,
+        },
+      });
+
+      throw err;
     }
-
-    const requestedTenant = fedApp.tenant;
-    const requestedProduct = fedApp.product;
-
-    const response = await this.ssoHandler.resolveConnection({
-      tenant: requestedTenant,
-      product: requestedProduct,
-      idp_hint,
-      authFlow: 'idp-initiated',
-      originalParams: { ...body },
-      tenants: fedApp.tenants,
-      idFedAppId: fedApp.id,
-      fedType: fedApp.type, // will be saml
-      thirdPartyLogin: { idpInitiatorType: 'oidc', iss, target_link_uri },
-    });
-
-    if ('connection' in response) {
-      connection = response.connection as OIDCSSORecord;
-    }
-
-    if (!connection) {
-      throw new JacksonError('IdP connection not found.', 404);
-    }
-
-    if (!isConnectionActive(connection)) {
-      throw new JacksonError('OIDC connection is deactivated. Please contact your administrator.', 403);
-    }
-
-    const requestParams = {
-      acsUrl: fedApp.acsUrl,
-      entityId: fedApp.entityId,
-      tenant: fedApp.tenant,
-      product: fedApp.product,
-      relayState: target_link_uri,
-    };
-
-    return await this.ssoHandler.createOIDCRequest({
-      connection,
-      requestParams,
-      mappings: fedApp.mappings,
-    });
   }
 }
