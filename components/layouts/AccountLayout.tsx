b60a42f652153d3c128622f0d4f6596577a568b2
Aswin V <vaswin91@gmail.com>
2024-09-26 15:50:56 +0530

    Remove whitespace in layout

--- components/layouts/AccountLayout.tsx
+++ components/layouts/AccountLayout.tsx
@@ -48,7 +48,7 @@ export const AccountLayout = ({ children }: { children: React.ReactNode }) => {
         </div>
         <main>
           <div className='py-6'>
-            <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>{children}</div>
+            <div className='mx-auto px-4 sm:px-6 md:px-8'>{children}</div>
           </div>
         </main>
       </div>
