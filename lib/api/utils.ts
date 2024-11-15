beacb54cbd4497294b7bee5b98842cb14a1cd984
Aswin V <vaswin91@gmail.com>
2024-11-15 19:02:41 +0530

    Fallback to true for unexpected values

--- lib/api/utils.ts
+++ lib/api/utils.ts
@@ -1,8 +1,7 @@
 export function normalizeBooleanParam(param) {
-  if (param === 'true' || param === true) {
-    return true;
-  }
   if (param === 'false' || param === false) {
     return false;
   }
+
+  return true;
 }
