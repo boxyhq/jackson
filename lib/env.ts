94872109f52ef8ec6c7d4e25fd21486253917473
Aswin V <vaswin91@gmail.com>
2025-01-02 21:00:24 +0530

    Pass pino logger from outside

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,7 @@ import type {
   JacksonOption,
   SSOTracesOption,
 } from '@boxyhq/saml-jackson';
+import { logger } from './logger';
 
 const samlPath = '/api/oauth/saml';
 const oidcPath = '/api/oauth/oidc';
@@ -127,6 +128,11 @@ const jacksonOptions: JacksonOption = {
   setupLinkExpiryDays,
   boxyhqHosted,
   ssoTraces,
+  logger: {
+    info: (msg: string, err?: any) => logger.info(err, msg),
+    error: (msg: string, err?: any) => logger.error(err, msg),
+    warn: (msg: string, err?: any) => logger.warn(err, msg),
+  },
 };
 
 const adminPortalSSODefaults = {
