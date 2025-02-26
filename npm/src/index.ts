cba18b5adcf541188d63fbcd0b1706a578807257
Aswin V <vaswin91@gmail.com>
2025-02-26 23:30:06 +0700

    [npm] Remove health check controller

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -10,7 +10,6 @@ import loadConnection from './loadConnection';
 import { AdminController } from './controller/admin';
 import { ConnectionAPIController } from './controller/api';
 import { OAuthController } from './controller/oauth';
-import { HealthCheckController } from './controller/health-check';
 import { LogoutController } from './controller/logout';
 import initDirectorySync from './directory-sync';
 import { OidcDiscoveryController } from './controller/oidc-discovery';
@@ -82,7 +81,6 @@ export const controllers = async (
   oauthController: OAuthController;
   adminController: AdminController;
   logoutController: LogoutController;
-  healthCheckController: HealthCheckController;
   setupLinkController: SetupLinkController;
   directorySyncController: IDirectorySyncController;
   oidcDiscoveryController: OidcDiscoveryController;
@@ -102,7 +100,6 @@ export const controllers = async (
   const sessionStore = db.store('oauth:session', opts.db.ttl);
   const codeStore = db.store('oauth:code', opts.db.ttl);
   const tokenStore = db.store('oauth:token', opts.db.ttl);
-  const healthCheckStore = db.store('_health:check');
   const setupLinkStore = db.store('setup:link');
   const certificateStore = db.store('x509:certificates');
   const settingsStore = db.store('portal:settings');
@@ -124,8 +121,6 @@ export const controllers = async (
     oryController,
   });
   const adminController = new AdminController({ connectionStore, ssoTraces });
-  const healthCheckController = new HealthCheckController({ healthCheckStore });
-  await healthCheckController.init();
   const setupLinkController = new SetupLinkController({ setupLinkStore, opts });
 
   // Create default certificate if it doesn't exist.
@@ -207,7 +202,6 @@ export const controllers = async (
     oauthController,
     adminController,
     logoutController,
-    healthCheckController,
     setupLinkController,
     directorySyncController,
     oidcDiscoveryController,
