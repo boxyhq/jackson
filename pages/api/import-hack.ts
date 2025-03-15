626ea4140daeac2cd9aeb1cdf2ea23af7bc607ee
Deepak Prabhakara <deepak@boxyhq.com>
2025-03-15 09:57:48 +0000

    import hack for jose

--- pages/api/import-hack.ts
+++ pages/api/import-hack.ts
@@ -1,8 +1,11 @@
 // Leave the openid-client import to get nextjs to leave the library in node_modules after build
 import * as dummy from 'openid-client';
+import * as jose from 'jose';
+
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   const unused = dummy; // eslint-disable-line @typescript-eslint/no-unused-vars
+  const unused2 = jose; // eslint-disable-line @typescript-eslint/no-unused-vars
   res.status(200).json({});
 }
