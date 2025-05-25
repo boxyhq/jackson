7dc38a3b0917b78dfcc8ddfba8c709ec52f834b5
Deepak Prabhakara <deepak@boxyhq.com>
2025-05-25 11:05:36 +0100

    Chore/hide items (#3950)
    * hide id fed tab in well known urls if env var is set
    
    * updated node

--- pages/well-known/index.tsx
+++ pages/well-known/index.tsx
@@ -1,11 +1,14 @@
 import { WellKnownURLs } from '@boxyhq/internal-ui';
-import type { NextPage, GetServerSidePropsContext } from 'next';
+import type { NextPage, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { adminPortal } from '@lib/env';
 
-const WellKnownURLsIndex: NextPage = () => {
+const WellKnownURLsIndex: NextPage<InferGetServerSidePropsType<typeof getStaticProps>> = ({
+  hideIdentityFederation,
+}) => {
   return (
     <div className='mx-auto max-w-5xl px-4 py-10'>
-      <WellKnownURLs />
+      <WellKnownURLs hideIdentityFederation={hideIdentityFederation} />
     </div>
   );
 };
@@ -13,6 +16,7 @@ const WellKnownURLsIndex: NextPage = () => {
 export async function getStaticProps({ locale }: GetServerSidePropsContext) {
   return {
     props: {
+      hideIdentityFederation: adminPortal.hideIdentityFederation,
       ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
     },
   };
