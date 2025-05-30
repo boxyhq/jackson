77113969f889c4b81d97086d71f6b6ab1ca997e1
Deepak Prabhakara <deepak@boxyhq.com>
2025-05-30 18:45:39 +0100

    cleanup of legacy integration

--- lib/env.ts
+++ lib/env.ts
@@ -113,7 +113,6 @@ const jacksonOptions: JacksonOption = {
   },
   setupLinkExpiryDays,
   boxyhqHosted,
-  ory: { projectId: process.env.ENTERPRISE_ORY_PROJECT_ID, sdkToken: process.env.ENTERPRISE_ORY_SDK_TOKEN },
   ssoTraces,
   flattenRawClaims: process.env.FLATTEN_RAW_CLAIMS === 'true',
 };
